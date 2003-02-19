f80848002aba7fd4176bbc2f06f9e4d6b9b2cb62
hen <hen@users.noreply.github.com>
2003-02-19 23:38:27 +0000

    A simple convert system and a custom properties object [copy of genjava ordered properties] which maintains order for a properties file and allows multiple values to exist.
    It stores the multiple values in a List object. This may not be the
    nicest way to get multiple objects as it always means having to do
    instanceof'ing.

---
+++ simple-jndi/src/java/org/osjava/convert/Convert.java
@@ -0,0 +1,14 @@
+package com.generationjava.convert;
+
+import org.apache.commons.lang.NumberUtils;
+
+public class Convert {
+
+
+    public static Object convert(String value, String type) {
+        if("number".equals(type)) {
+            return NumberUtils.createNumber(value);
+        }
+    }
+
+}
---
+++ simple-jndi/src/java/org/osjava/jndi/util/CustomProperties.java
@@ -0,0 +1,117 @@
+/// TODO: Refactor this out
+package com.generationjava.jndi.util;
+
+import java.io.*;
+import java.util.*;
+import com.generationjava.util.OrderedProperties;
+
+public class CustomProperties extends OrderedProperties {
+
+    public synchronized void load(InputStream in) throws IOException {
+        try {
+            BufferedReader reader = new BufferedReader( new InputStreamReader(in) );
+            String line = "";
+            String nextLine = null;
+            while( (line = reader.readLine()) != null) {
+
+                // we may already be on a multi-line statement.
+                if(nextLine != null) {
+                    line = nextLine + line;
+                    nextLine = null;
+                }
+
+                line = line.trim();
+                if(line.endsWith("\\")) {
+                    nextLine = line;
+                    continue;
+                }
+
+                int idx = line.indexOf('#');
+                // remove comment
+                if(idx != -1) {
+                    line = line.substring(0,idx);
+                }
+                // split equals sign
+                idx = line.indexOf('=');
+                if(idx != -1) {
+                    this.setProperty(line.substring(0,idx), line.substring(idx+1));
+                } else {
+                    // blank line, or just a bad line
+                    // we ignore it
+                }
+            }
+            reader.close();
+            return props;
+        } catch(IOException ioe) {
+            ioe.printStackTrace();
+            return props;
+        }
+    }
+
+    public synchronized Object put(Object key, Object value) {
+        if(index.contains(key)) {
+            Object obj = get(key);
+            if( !(obj instanceof List)) {
+                List list = new LinkedList();
+                list.add(obj);
+                obj = list;
+            } 
+            ((List)obj).add(value);
+            value = obj;
+        }
+        if(!index.contains(key)) {
+            index.add(key);
+        }
+        return super.put(key, value);
+    }
+
+    // our index for the ordering
+    protected ArrayList index = new ArrayList();
+
+    public CustomProperties() {
+        super();
+    }
+
+    // the props attribute is for defaults. These will need to be 
+    // remembered for the save/store method.
+    public OrderedProperties(Properties props) {
+        super(props);
+    }
+
+    public synchronized Object setProperty(String key, String value) {
+        return put(key,value);
+    }
+    
+    public synchronized Object remove(Object key) {
+        index.remove(key);
+        return super.remove(key);
+    }
+    
+    // simple implementation that depends on keySet.
+    public synchronized Enumeration propertyNames() {
+        return IteratorUtils.asEnumeration(keySet().iterator());
+    }
+    public synchronized Enumeration keys() {
+        return propertyNames();
+    }
+    
+    public synchronized Set keySet() {
+        return new OrderedSet(index);
+    }
+ 
+    /**
+     * Currently will write out defaults as well, which is not 
+     * in the specification.
+     */
+    public void save(OutputStream outstrm, String header) {
+        super.save(outstrm,header);
+    }
+    /**
+     * Currently will write out defaults as well, which is not 
+     * in the specification.
+     */
+    public void store(OutputStream outstrm, String header) throws IOException {
+        super.store(outstrm,header);
+    }
+
+}
