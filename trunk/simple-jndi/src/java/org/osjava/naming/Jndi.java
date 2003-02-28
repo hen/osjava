329082d2fc8735dfe963ab2d6b6de9621bb4798e
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2003-02-28 03:37:08 +0000

    Added in lookupList and the general lookup method

--- trunk/simple-jndi/src/java/org/osjava/naming/Jndi.java
+++ trunk/simple-jndi/src/java/org/osjava/naming/Jndi.java
@@ -1,5 +1,8 @@
 package com.generationjava.naming;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.sql.DataSource;
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -13,12 +16,26 @@ public class Jndi {
         context = new InitialContext();
     }
 
+    public Object lookup(String key) throws NamingException {
+        return this.context.lookup(key);
+    }
+
     public DataSource lookupDataSource(String key) throws NamingException {
         return (DataSource)this.context.lookup(key);
     }
 
     public String lookupString(String key) throws NamingException {
         return (String)this.context.lookup(key);
+    }
+
+    public List lookupList(String key) throws NamingException {
+        Object obj = this.context.lookup(key);
+        if( !(obj instanceof List) ) {
+            List list = new ArrayList(1);
+            list.add(obj);
+            obj = list;
+        }
+        return (List)obj;
     }
 
 }
