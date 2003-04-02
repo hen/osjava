6894b91e69ea095ceec22368123bf3c1910ac1bf
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2003-04-02 20:42:09 +0000

    Initial revision

---
+++ trunk/runsql/example/sql/BOOTSTRAP.sql
@@ -0,0 +1,4 @@
+INSERT INTO Report (name, sql, age) VALUES ('foo', 'select foo', '95');
+INSERT INTO Report (name, sql, age) VALUES ('bar', 'select poo', '5');
+INSERT INTO Report (name, sql, age) VALUES ('poo', 'delete foo', '9');
+INSERT INTO Report (name, sql, age) VALUES ('barry', 'select wfoo', '9191');
