535bead5c0e71d96169d7e5137acee27bc173831
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2003-04-07 19:16:15 +0000

    Fits the new interface.

--- trunk/scraping-engine/src/java/org/osjava/scraping/NullFetcher.java
+++ trunk/scraping-engine/src/java/org/osjava/scraping/NullFetcher.java
@@ -2,7 +2,7 @@ package org.osjava.scraping;
 
 public class NullFetcher implements Fetcher {
 
-    public Page fetch(Config cfg, Session session) throws FetchingException {
+    public Page fetch(String uri, Config cfg, Session session) throws FetchingException {
         return new NullPage();
     }
 
