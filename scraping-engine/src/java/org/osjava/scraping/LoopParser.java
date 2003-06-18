2f4e6e69cf4aa32006ef8e18313bb7651f96689f
hen <hen@users.noreply.github.com>
2003-06-18 22:06:11 +0000

    Changed the name of project from Simple-JNDI to Scabies

--- scraping-engine/src/java/org/osjava/scraping/LoopParser.java
+++ scraping-engine/src/java/org/osjava/scraping/LoopParser.java
@@ -13,7 +13,7 @@
  *   this list of conditions and the following disclaimer in the documentation 
  *   and/or other materials provided with the distribution.
  * 
- * + Neither the name of Simple-JNDI nor the names of its contributors 
+ * + Neither the name of Scabies nor the names of its contributors 
  *   may be used to endorse or promote products derived from this software 
  *   without specific prior written permission.
  * 
