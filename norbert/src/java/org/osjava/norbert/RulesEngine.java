0483542b96ee3d8dd8d5f4b768a57e32cf9f8386
hen <hen@users.noreply.github.com>
2003-08-23 06:16:59 +0000

    Added License and javadoc.

--- norbert/src/java/org/osjava/norbert/RulesEngine.java
+++ norbert/src/java/org/osjava/norbert/RulesEngine.java
@@ -1,3 +1,34 @@
+/*
+ * Copyright (c) 2003, Henri Yandell
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or 
+ * without modification, are permitted provided that the 
+ * following conditions are met:
+ * 
+ * + Redistributions of source code must retain the above copyright notice, 
+ *   this list of conditions and the following disclaimer.
+ * 
+ * + Redistributions in binary form must reproduce the above copyright notice, 
+ *   this list of conditions and the following disclaimer in the documentation 
+ *   and/or other materials provided with the distribution.
+ * 
+ * + Neither the name of OSJava nor the names of its contributors 
+ *   may be used to endorse or promote products derived from this software 
+ *   without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.osjava.norbert;
 
 import java.util.ArrayList;
@@ -8,7 +39,8 @@ import java.util.List;
  * Contains a series of Rules. It then runs a path against these 
  * to decide if it is allowed or not. 
  */
-public class RulesEngine {
+ // TODO: Make this package private
+class RulesEngine {
 
     private List rules;
 
@@ -28,6 +60,12 @@ public class RulesEngine {
         this.rules.add(rule);
     }
 
+    /**
+     * Run each Rule in series on the path. 
+     * If a Rule returns a Boolean, return that.
+     * If null is returned, move on.
+     * When no more rules are left, return true. 
+     */
     public boolean isAllowed(String path) {
 
         Iterator iterator = this.rules.iterator();
