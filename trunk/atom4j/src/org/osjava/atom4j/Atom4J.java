5354a827ba96966e94d3b1ed2e9239203731242e
lavandowska <lavandowska@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2003-08-29 19:13:20 +0000

    *** empty log message ***

---
+++ trunk/atom4j/src/org/osjava/atom4j/Atom4J.java
@@ -0,0 +1,16 @@
+/*
+ * http://bitworking.org/news/AtomAPI_URIs
+ * http://bitworking.org/rfc/draft-gregorio-07.html
+ * 
+ * Created on Aug 29, 2003
+ */
+package org.osjava.atom4j;
+
+/**
+ * @author llavandowska
+ */
+public abstract class Atom4J
+{
+
+    public static String xmlns = "http://example.com/newformat#";
+}
