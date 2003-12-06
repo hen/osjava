ca6b7d36a41aaa1bac0d1082facc561ee00ecc87
hen <hen@users.noreply.github.com>
2003-12-06 23:47:00 +0000

    added an xml-datasource test

--- simple-jndi/src/test/org/osjava/jndi/LookupTest.java
+++ simple-jndi/src/test/org/osjava/jndi/LookupTest.java
@@ -12,148 +12,162 @@ import junit.textui.TestRunner;
 
 public class LookupTest extends TestCase {
 
-    private String delimiter = ".";
+    private InitialContext ctxt;
+    private String delimiter;
 
     public LookupTest(String name) {
         super(name);
     }
 
+    public void setUp() {
+        try {
+            ctxt = new InitialContext();
+            delimiter = (String) ctxt.lookup("org.osjava.jndi.delimiter");
+        } catch(NamingException ne) {
+            ne.printStackTrace();
+        }
+    }
+
+    public void tearDown() {
+        this.ctxt = null;
+        this.delimiter = null;
+    }
+
     public void testDSLookup() {
         try {
-            InitialContext ctxt = new InitialContext();
-            this.delimiter = (String) ctxt.lookup("org.osjava.jndi.delimiter");
-
             Properties props = new Properties();
             props.setProperty("TestDS/url", "foofoo");
             props.setProperty("TestDS/driver", "bing");
             props.setProperty("TestDS/user", "Boo");
             props.setProperty("TestDS/password", "bong");
             props.setProperty("org.osjava.jndi.datasource", "true");
-            PropertiesDataSource fake = new PropertiesDataSource(props, new Hashtable(), this.delimiter);
+            PropertiesDataSource fake = new PropertiesDataSource(props, new Hashtable(), delimiter);
             fake.setName("TestDS");
-            assertEquals( fake, lookup("java:/TestDS", ctxt) );
+            assertEquals( fake, lookup("java:/TestDS") );
         } catch(NamingException ne) {
-            ne.printStackTrace();
+            fail("NamingException: "+ne.getMessage());
         }
     }
 
     public void testValueLookup() {
         try {
-            InitialContext ctxt = new InitialContext();
-            this.delimiter = (String) ctxt.lookup("org.osjava.jndi.delimiter");
-
-            assertEquals( "13", lookup("test/value", ctxt) );
+            assertEquals( "13", lookup("test/value") );
         } catch(NamingException ne) {
-            ne.printStackTrace();
+            fail("NamingException: "+ne.getMessage());
         }
     }
 
     public void testValuesLookup() {
         try {
-            InitialContext ctxt = new InitialContext();
-            this.delimiter = (String) ctxt.lookup("org.osjava.jndi.delimiter");
-
             HashMap map = new HashMap();
             map.put("five", "5");
             map.put("one", "two");
             map.put("three", "four");
-            assertEquals( map, lookup("thing/type", ctxt) );
+            assertEquals( map, lookup("thing/type") );
         } catch(NamingException ne) {
-            ne.printStackTrace();
+            fail("NamingException: "+ne.getMessage());
         }
     }
 
     public void testDS2Lookup() {
         try {
-            InitialContext ctxt = new InitialContext();
-            this.delimiter = (String) ctxt.lookup("org.osjava.jndi.delimiter");
-
-            DataSource aTestDS = (DataSource) lookup("thing.db.ATestDS", ctxt);
-            Properties props1 = new Properties();
-            // even though delimiter is a /, we use . here for the 
-            // test. This is because the dot needs to be in the file
-            props1.setProperty("ATestDS/url", "jdbc:mysql://192.168.133.2/bikehell");
-            props1.setProperty("ATestDS/driver", "org.gjt.mm.mysql.Driver");
-            props1.setProperty("ATestDS/user", "nico");
-            props1.setProperty("ATestDS/password", "bear");
-            props1.setProperty("org.osjava.jndi.datasource", "true");
-            PropertiesDataSource fake1 = new PropertiesDataSource(props1, new Hashtable(), this.delimiter);
-            fake1.setName("ATestDS");
-            assertEquals( fake1, aTestDS );
+            DataSource aTestDS = (DataSource) lookup("thing.db.ATestDS");
+            DataSource fake = createFakeDS(
+            "jdbc:mysql://192.168.133.2/bikehell", 
+            "org.gjt.mm.mysql.Driver", 
+            "nico", 
+            "bear","ATestDS");
+            assertEquals( fake, aTestDS );
         } catch(NamingException ne) {
-            ne.printStackTrace();
+            fail("NamingException: "+ne.getMessage());
         }
     }
             
     public void testListLookup() {
         try {
-            InitialContext ctxt = new InitialContext();
-            this.delimiter = (String) ctxt.lookup("org.osjava.jndi.delimiter");
-
             ArrayList list = new ArrayList();
             list.add( new Integer(24) );
             list.add( new Integer(25) );
             list.add( new Integer(99) );
-            assertEquals( list, lookup("thing.type.bob.age", ctxt) );
+            assertEquals( list, lookup("thing.type.bob.age") );
         } catch(NamingException ne) {
-            ne.printStackTrace();
+            fail("NamingException: "+ne.getMessage());
         }
     }
 
     public void testList2Lookup() {
         try {
-            InitialContext ctxt = new InitialContext();
-            this.delimiter = (String) ctxt.lookup("org.osjava.jndi.delimiter");
-
             ArrayList list2 = new ArrayList();
             list2.add( "Henri" );
             list2.add( "Fred" );
-            assertEquals( list2, lookup("name", ctxt) );
-            assertEquals( "yandell.org", lookup("url", ctxt) );
-            assertEquals( "Foo", ctxt.lookup("com.genjava") );
+            assertEquals( list2, lookup("name") );
+            assertEquals( "yandell.org", lookup("url") );
+            assertEquals( "Foo", this.ctxt.lookup("com.genjava") );
         } catch(NamingException ne) {
-            ne.printStackTrace();
+            fail("NamingException: "+ne.getMessage());
         }
     }
 
     public void testDS3Lookup() {
         try {
-            InitialContext ctxt = new InitialContext();
-            this.delimiter = (String) ctxt.lookup("org.osjava.jndi.delimiter");
-
-            DataSource genscapeDS = (DataSource) lookup("GenscapeDS", ctxt);
-            Properties props2 = new Properties();
-            props2.setProperty("url", "jdbc:mysql://192.168.133.2/bikehell");
-            props2.setProperty("driver", "org.gjt.mm.mysql.Driver");
-            props2.setProperty("user", "nico");
-            props2.setProperty("password", "bear");
-            props2.setProperty("org.osjava.jndi.datasource", "true");
-            PropertiesDataSource fake2 = new PropertiesDataSource(props2, new Hashtable(), this.delimiter);
-            fake2.setName("");
-            assertEquals( fake2, genscapeDS );
+            DataSource genscapeDS = (DataSource) lookup("GenscapeDS");
+            Properties props = new Properties();
+            props.setProperty("url", "jdbc:mysql://192.168.133.2/bikehell");
+            props.setProperty("driver", "org.gjt.mm.mysql.Driver");
+            props.setProperty("user", "nico");
+            props.setProperty("password", "bear");
+            props.setProperty("org.osjava.jndi.datasource", "true");
+            PropertiesDataSource fake = new PropertiesDataSource(props, new Hashtable(), delimiter);
+            fake.setName("");
+            assertEquals( fake, genscapeDS );
         } catch(NamingException ne) {
-            ne.printStackTrace();
+            fail("NamingException: "+ne.getMessage());
         }
     }
 
     public void testXmlLookup() {
         try {
-            InitialContext ctxt = new InitialContext();
-            this.delimiter = (String) ctxt.lookup("org.osjava.jndi.delimiter");
-
-//            System.err.println("XML: "+lookup("xmltest", ctxt) );
+//            System.err.println("XML: "+lookup("xmltest") );
 // TODO: Should this return something? XML?
-//            System.err.println("XML: "+lookup("xmltest/config", ctxt) );
-            assertEquals( "13", lookup("xmltest/config/value", ctxt) );
-            assertEquals( "Bang", lookup("xmltest/config/four/five", ctxt) );
-            assertEquals( "three", lookup("xmltest/config/one/two", ctxt) );
+//            System.err.println("XML: "+lookup("xmltest.config") );
+            assertEquals( "13", lookup("xmltest.config.value") );
+            assertEquals( "Bang", lookup("xmltest.config.four.five") );
+            assertEquals( "three", lookup("xmltest.config.one.two") );
         } catch(NamingException ne) {
             fail("NamingException: "+ne.getMessage());
         }
     }
 
 
-    private Object lookup(String key, Context ctxt) throws NamingException {
-        return ctxt.lookup( StringUtils.replace(key, ".", delimiter) );
+    public void testXmlDSLookup() {
+        try {
+            DataSource aTestDS = (DataSource) lookup("thing.db2.ATestDS");
+            DataSource fake = createFakeDS(
+            "jdbc:mysql://192.168.133.2/bikehell", 
+            "org.gjt.mm.mysql.Driver", 
+            "nico", 
+            "bear","ATestDS");
+            assertEquals( fake, aTestDS );
+
+        } catch(NamingException ne) {
+            fail("NamingException: "+ne.getMessage());
+        }
+    }
+
+    private DataSource createFakeDS(String url, String driver, String user, String passwd, String name) {
+        Properties props = new Properties();
+        // even though delimiter is a /, we use . here for the 
+        // test. This is because the dot needs to be in the file
+        props.setProperty(name+"/url", url);
+        props.setProperty(name+"/driver", driver);
+        props.setProperty(name+"/user", user);
+        props.setProperty(name+"/password", passwd);
+        PropertiesDataSource fake = new PropertiesDataSource(props, new Hashtable(), this.delimiter);
+        fake.setName(name);
+        return fake;
+    }
+
+    private Object lookup(String key) throws NamingException {
+        return this.ctxt.lookup( StringUtils.replace(key, ".", this.delimiter) );
     }
 }
