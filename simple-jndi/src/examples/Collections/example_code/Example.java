a63d792855437c67ca6f199d21ba0befb67d93d4
hen <hen@users.noreply.github.com>
2004-01-03 23:11:32 +0000

    added classcast handling to Example, though I need to find some way to imply this code is not absoltely necessary but just a nice check when you are having problems

--- simple-jndi/src/examples/Collections/example_code/Example.java
+++ simple-jndi/src/examples/Collections/example_code/Example.java
@@ -11,8 +11,12 @@ public class Example {
     public static void main(String[] args) {
         try {
             Context ctxt =  new InitialContext();
-            List list = (List) ctxt.lookup("shopping/item");
-            System.out.println("Items are: "+list);
+            try {
+                List list = (List) ctxt.lookup("shopping/item");
+                System.out.println("Items are: "+list);
+            } catch(ClassCastException cce) {
+                System.err.println("List not created, instead made: "+ctxt.lookup("shopping/item"));
+            }
         } catch(NamingException ne) {
             System.err.println("NamingException: "+ne.getMessage());
         }
