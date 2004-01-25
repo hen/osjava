1390955b3993c99af97f9bd8e78763f157bf427c
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2004-01-25 06:58:17 +0000

    more tests added

--- trunk/genjava/gj-csv/src/test/com/generationjava/io/CsvWriterTest.java
+++ trunk/genjava/gj-csv/src/test/com/generationjava/io/CsvWriterTest.java
@@ -15,6 +15,8 @@ public class CsvWriterTest extends TestCase {
         try {
             StringWriter sw = new StringWriter();
             CsvWriter csv = new CsvWriter(sw);
+            csv.setBlockDelimiter('\n');
+            csv.setFieldDelimiter(',');
             csv.writeField("1");
             csv.writeField("2");
             csv.writeField("3");
@@ -30,6 +32,42 @@ public class CsvWriterTest extends TestCase {
             csv.close();
             assertEquals("Does not write simple csv file out correctly. ", 
                          "1,2,3,4,5\n6,7,8,9,0\n",
+                         sw.toString() );
+            assertEquals("Does not return the right Writer", sw, csv.getWriter() );
+        } catch(IOException ioe) {
+            fail("IOException should not have been thrown. ");
+        }
+    }
+
+    public void testWriteLines() {
+        try {
+            StringWriter sw = new StringWriter();
+            CsvWriter csv = new CsvWriter(sw);
+            String[] strs = new String[] { "1", "2", "3", "4", "5" };
+            csv.writeLine(strs);
+            strs = new String[] { "6", "7", "8", "9", "0" };
+            csv.writeLine(strs);
+            csv.close();
+            assertEquals("Does not write simple csv file out correctly. ", 
+                         "1,2,3,4,5\n6,7,8,9,0\n",
+                         sw.toString() );
+        } catch(IOException ioe) {
+            fail("IOException should not have been thrown. ");
+        }
+    }
+
+    public void testEscapeQuotes() {
+        try {
+            StringWriter sw = new StringWriter();
+            CsvWriter csv = new CsvWriter(sw);
+            String[] strs = new String[] { "That's \"Mr. Monkey\", to you", "Fool" };
+            csv.writeLine(strs);
+            strs = new String[] { "Not \"Monkey\"", "Fool" };
+            csv.writeLine(strs);
+            csv.close();
+            assertEquals("Does not write simple csv file out correctly. ", 
+                         "\"That's \"\"Mr. Monkey\"\" to you\",Fool\n" +
+                         "Not \"\"Monkey\",Fool\n",
                          sw.toString() );
         } catch(IOException ioe) {
             fail("IOException should not have been thrown. ");
