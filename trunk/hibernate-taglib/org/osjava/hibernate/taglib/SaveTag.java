40d03a489ff41e3a740d0bba693edef946d6cd64
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2004-01-27 07:30:45 +0000

    Added tags to create a new bean in hibernate, and to save an existing one. However the save seems to not be too hot currently.

---
+++ trunk/hibernate-taglib/org/osjava/hibernate/taglib/SaveTag.java
@@ -0,0 +1,99 @@
+package org.osjava.hibernate.taglib;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+import java.util.List;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Enumeration;
+
+import javax.servlet.ServletRequest;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspTagException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.TagSupport;
+
+import net.sf.hibernate.SessionFactory;
+import net.sf.hibernate.Session;
+import net.sf.hibernate.cfg.Configuration;
+import net.sf.hibernate.HibernateException;
+import net.sf.hibernate.Hibernate;
+import net.sf.hibernate.type.Type;
+
+import org.apache.commons.beanutils.BeanUtils;
+
+import java.lang.reflect.InvocationTargetException;
+
+// Make this need a DataSource
+public class SaveTag extends TagSupport {
+
+    private Object value;
+
+    public SaveTag() {
+    }
+
+    public Object getValue() { return this.value; }
+    public void setValue(Object value) { this.value = value; }
+
+    public int doEndTag() throws JspException {
+        Session hSession = null;
+
+        try {
+            SessionFactory hSf = new Configuration().configure().buildSessionFactory();
+            hSession = hSf.openSession();
+
+            if(value instanceof String) {
+                throw new JspTagException("Bad to not have an Object value. ");
+            }
+            Object bean = this.value;
+
+            System.err.println("BEAN: "+bean);
+
+            /* TO DECIDE IF THIS IS DESIRED. SHOULD save MODIFY THEN SAVE?
+            try {
+                // copy values from request onto the bean
+                BeanUtils.populate(bean, pageContext.getRequest().getParameterMap());
+                // copy values from request scope onto the bean
+                BeanUtils.populate(bean, attributesToMap(pageContext.getRequest()));
+            } catch(IllegalAccessException iae) {
+                // hmmmh...
+                iae.printStackTrace();
+            } catch(InvocationTargetException ite) {
+                // hmmmh...
+                ite.printStackTrace();
+            }
+            */
+
+            hSession.save(bean);
+        } catch(HibernateException he) {
+            he.printStackTrace();
+        } catch(Throwable t) {
+            t.printStackTrace();
+        } finally {
+            if(hSession != null) {
+                try {
+                    hSession.close();
+                } catch(HibernateException he) {
+                    // ignore
+                }
+            }
+        }
+
+        return EVAL_PAGE;
+    }
+
+    private Map attributesToMap(ServletRequest request) {
+        HashMap map = new HashMap();
+        Enumeration enum = request.getAttributeNames();
+        while(enum.hasMoreElements()) {
+            String key = (String) enum.nextElement();
+            map.put( key, request.getAttribute( key ) );
+        }
+        return map;
+    }
+
+}
