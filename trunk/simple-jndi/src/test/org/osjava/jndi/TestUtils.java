1f27e5ba9cc3ff66353a1639022307d61af1b38b
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2004-02-11 01:25:24 +0000

    fixing some minor compilation problems

--- trunk/simple-jndi/src/test/org/osjava/jndi/TestUtils.java
+++ trunk/simple-jndi/src/test/org/osjava/jndi/TestUtils.java
@@ -2,10 +2,11 @@ package org.osjava.jndi;
 
 import java.util.Properties;
 import java.util.Hashtable;
+import javax.sql.DataSource;
 
-public class TestUtils {
+class TestUtils {
 
-    DataSource createFakeDS(String url, String driver, String user, String passwd, String name) {
+    static DataSource createFakeDS(String url, String driver, String user, String passwd, String name, String delimiter) {
         Properties props = new Properties();
         // even though delimiter is a /, we use . here for the 
         // test. This is because the dot needs to be in the file
@@ -13,7 +14,7 @@ public class TestUtils {
         props.setProperty(name+"/driver", driver);
         props.setProperty(name+"/user", user);
         props.setProperty(name+"/password", passwd);
-        PropertiesDataSource fake = new PropertiesDataSource(props, new Hashtable(), this.delimiter);
+        PropertiesDataSource fake = new PropertiesDataSource(props, new Hashtable(), delimiter);
         fake.setName(name);
         return fake;
     }
