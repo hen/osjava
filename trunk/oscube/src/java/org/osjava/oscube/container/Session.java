99ce05ed6d2325ae7d53c48bd3cb6a4c8c2a7207
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2004-02-17 06:02:43 +0000

    removed repeated methods

--- trunk/oscube/src/java/org/osjava/oscube/container/Session.java
+++ trunk/oscube/src/java/org/osjava/oscube/container/Session.java
@@ -35,9 +35,6 @@ import java.util.List;
 
 public interface Session extends Config {
 
-    public Object get(String key);
-    public String getString(String key);
-    public List getList(String key);
     public Object remove(String key);
 
     public void put(String key, Object value);
