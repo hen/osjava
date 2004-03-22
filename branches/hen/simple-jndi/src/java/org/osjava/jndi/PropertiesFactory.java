2b3426fa94b540141fdb985fab654d02ed926172
unknown <unknown@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2004-03-22 17:14:33 +0000

    This commit was manufactured by cvs2svn to create branch 'hen'.

---
+++ branches/hen/simple-jndi/src/java/org/osjava/jndi/PropertiesFactory.java
@@ -0,0 +1,16 @@
+package org.apache.commons.dbutils.jndi;
+
+import javax.naming.spi.InitialContextFactory;
+import javax.naming.Context;
+import java.util.Hashtable;
+
+public class PropertiesFactory implements InitialContextFactory {
+
+    public PropertiesFactory() {
+        super();
+    }
+
+    public Context getInitialContext(Hashtable environment) {
+        return new PropertiesContext(environment);
+    }
+}
