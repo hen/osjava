c234271c2a2ad315a3de1f631156d5518548b976
unknown <unknown@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2004-03-22 17:15:29 +0000

    This commit was manufactured by cvs2svn to create branch 'hen'.

---
+++ branches/hen/genjava/gj-scrape/src/test/com/generationjava/scrape/HtmlScraperTest.java
@@ -0,0 +1,50 @@
+package com.generationjava.scrape;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+
+public class HtmlScraperTest extends TestCase {
+
+    public final String TEST_PAGE = "<html><body><table bgcolor=\"ffffff\"><tr><td>FOO</td></tr></table></body></html>";
+
+    public HtmlScraperTest(String name) {
+        super(name);
+    }
+
+    //-----------------------------------------------------------------------
+    // To test: 
+    //   HtmlScraper()->scrape(..)->get/move etc
+
+    public void testScrape() {
+        HtmlScraper scraper = new HtmlScraper();
+        scraper.scrape(TEST_PAGE);
+        assertEquals( "FOO", scraper.get("td") );
+    }
+
+    public void testScrapeAttribute() {
+        HtmlScraper scraper = new HtmlScraper();
+        scraper.scrape(TEST_PAGE);
+        assertEquals( "ffffff", scraper.get("table[bgcolor]") );
+    }
+
+    public void testCaseInsensitive() {
+        HtmlScraper scraper = new HtmlScraper();
+        scraper.scrape(TEST_PAGE);
+        assertEquals( "FOO", scraper.get("TD") );
+        assertEquals( "FOO", scraper.get("Td") );
+        assertEquals( "FOO", scraper.get("tD") );
+        assertEquals( "FOO", scraper.get("td") );
+    }
+    
+    public void testCaseInsensitiveAttribute() {
+        HtmlScraper scraper = new HtmlScraper();
+        scraper.scrape(TEST_PAGE);
+        assertEquals( "ffffff", scraper.get("TABLE[bgcolor]") );
+        assertEquals( "ffffff", scraper.get("TABLE[BGCOLOR]") );
+        assertEquals( "ffffff", scraper.get("table[BGCOLOR]") );
+        assertEquals( "ffffff", scraper.get("table[bgcolor]") );
+    }
+
+}
