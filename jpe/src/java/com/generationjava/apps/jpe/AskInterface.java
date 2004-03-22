50d97fe3ed42deeba0154f8da756b3d368a38beb
unknown <unknown@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2004-03-22 17:17:11 +0000

    This commit was manufactured by cvs2svn to create tag 'CVSROOT-test-taginfo'.

--- jpe/src/java/com/generationjava/apps/jpe/AskInterface.java
+++
@@ -1,9 +0,0 @@
-package com.generationjava.apps.jpe;
-
-/**
-* ask interface is used as a callback system for input dialog handler
-* its using a callback so i don't have to use a extra thread
-*/
-interface AskInterface {
-        public void askCallback(String command,String result);
-}
--- runsql/example/sql/BOOTSTRAP.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Report (name, sql, age) VALUES ('foo', 'select foo', '95');
-INSERT INTO Report (name, sql, age) VALUES ('bar', 'select poo', '5');
-INSERT INTO Report (name, sql, age) VALUES ('poo', 'delete foo', '9');
-INSERT INTO Report (name, sql, age) VALUES ('barry', 'select wfoo', '9191');
