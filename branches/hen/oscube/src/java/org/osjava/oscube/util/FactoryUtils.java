9cc8fda9c43fcbaaba7ec07acecc31856146eb5b
unknown <unknown@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2004-03-22 17:16:23 +0000

    This commit was manufactured by cvs2svn to create branch 'hen'.

---
+++ branches/hen/oscube/src/java/org/osjava/oscube/util/FactoryUtils.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2003, Henri Yandell
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or 
+ * without modification, are permitted provided that the 
+ * following conditions are met:
+ * 
+ * + Redistributions of source code must retain the above copyright notice, 
+ *   this list of conditions and the following disclaimer.
+ * 
+ * + Redistributions in binary form must reproduce the above copyright notice, 
+ *   this list of conditions and the following disclaimer in the documentation 
+ *   and/or other materials provided with the distribution.
+ * 
+ * + Neither the name of Scabies nor the names of its contributors 
+ *   may be used to endorse or promote products derived from this software 
+ *   without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.osjava.oscube.util;
+
+import com.generationjava.lang.ClassW;
+
+public class FactoryUtils {
+
+    static private String NULL_NAME = "Null";
+
+    static public Object getObject(String name, String type, String defaultPackage) {
+        if(name != null) {
+            Object obj = ClassW.createObject(defaultPackage+"."+name+type);
+            // should it check the type? it assumes we have a 
+            // clue what the interface is...
+            if(obj != null) {
+                return obj;
+            }
+            obj = ClassW.createObject(name+type);
+            if(obj != null) {
+                return obj;
+            }
+            obj = ClassW.createObject(name);
+            if(obj != null) {
+                return obj;
+            }
+        }
+        if(name == NULL_NAME) {
+            // if it couldn't find the Null pattern for the type
+            return null;
+        } else {
+            return getObject(NULL_NAME, type, defaultPackage);
+        }
+    }
+
+}
