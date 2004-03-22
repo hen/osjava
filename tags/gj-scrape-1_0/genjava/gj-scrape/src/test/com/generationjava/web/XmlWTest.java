69655d82f21283fb72cfec0054fe3779020c9e70
unknown <unknown@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2004-03-22 17:17:09 +0000

    This commit was manufactured by cvs2svn to create tag 'gj-scrape-1_0'.

---
+++ tags/gj-scrape-1_0/genjava/gj-scrape/src/test/com/generationjava/web/XmlWTest.java
@@ -0,0 +1,28 @@
+package com.generationjava.web;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+
+public class XmlWTest extends TestCase {
+
+    public XmlWTest(String name) {
+        super(name);
+    }
+
+    //-----------------------------------------------------------------------
+    // To test: 
+    //     XmlW.removeXml
+    //     XmlW.getContent
+
+    public void testRemove() {
+        assertEquals( "FOO", XmlW.removeXml("<foo>FOO</foo>") );
+        assertEquals( "FOO", XmlW.removeXml("<foo test='blah'>FOO</foo>") );
+        assertEquals( "FOO", XmlW.removeXml("<foo><blah/>FOO</foo>") );
+        assertEquals( "FOO", XmlW.removeXml("FOO") );
+        assertEquals( "some comment", XmlW.removeXml("<h2>some comment</h2>") );
+        assertEquals( "", XmlW.removeXml("") );
+    }
+
+}
