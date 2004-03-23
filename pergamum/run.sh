5f2e7ab6e021db09eb35f0c9898dc962b167a2fc
hen <hen@users.noreply.github.com>
2004-03-23 06:16:47 +0000

    generates more files now

--- pergamum/run.sh
+++ pergamum/run.sh
@@ -1,4 +1,10 @@
-for xml in comingsoon.xml listbooks.xml listcategories.xml listpublishers.xml newbooks.xml newreviews.xml tmpbook.xml
+java Style  content/book.xml pergamum.xsl db/books.xml book isbn
+java Style  content/publisher-books.xml pergamum.xsl db/publishers.xml publisher id
+java Style  content/category-books.xml pergamum.xsl db/categories.xml category id
+
+for xml in comingsoon.xml listbooks.xml listcategories.xml listpublishers.xml newbooks.xml newreviews.xml 
 do
     java Style content/$xml pergamum.xsl
 done
+
+java Style content/index.xml pergamum.xsl
