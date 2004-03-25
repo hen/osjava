96425a42252be49d07b596ae670e3c2c23d47d89
hen <hen@users.noreply.github.com>
2004-03-25 04:23:42 +0000

    linked help in

--- pergamum/run.sh
+++ pergamum/run.sh
@@ -2,7 +2,7 @@ java Style  content/book.xml pergamum.xsl db/books.xml book isbn
 java Style  content/publisher-books.xml pergamum.xsl db/publishers.xml publisher id
 java Style  content/category-books.xml pergamum.xsl db/categories.xml category id
 
-for xml in comingsoon.xml listbooks.xml listcategories.xml listpublishers.xml newbooks.xml newreviews.xml 
+for xml in comingsoon.xml listbooks.xml listcategories.xml listpublishers.xml newbooks.xml newreviews.xml help.xml
 do
     java Style content/$xml pergamum.xsl
 done
