b14c043bfb32b01c3e972101dd7b7cf24a490f32
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2004-03-29 04:31:00 +0000

    compile the Style.java every time to put a hurting on the cpu and make my life one tiny iota easier

--- trunk/pergamum/run.sh
+++ trunk/pergamum/run.sh
@@ -1,3 +1,4 @@
+javac Style.java
 java Style  content/book.xml pergamum.xsl db/books.xml book isbn
 java Style  content/publisher-books.xml pergamum.xsl db/publishers.xml publisher id
 java Style  content/category-books.xml pergamum.xsl db/categories.xml category id
