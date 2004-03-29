e61996794a2aa765ba1b4c3bd557cabc6e8038ff
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2004-03-29 21:33:33 +0000

    renamed pergamum.xsl as I think I will have one for html and one for rss, though the rss one can be a subset of the html one as only a few pages will be done

--- trunk/pergamum/run.sh
+++ trunk/pergamum/run.sh
@@ -1,11 +1,11 @@
 javac Style.java
-java Style  content/book.xml pergamum.xsl db/books.xml book isbn
-java Style  content/publisher-books.xml pergamum.xsl db/publishers.xml publisher id
-java Style  content/category-books.xml pergamum.xsl db/categories.xml category id
+java Style  content/book.xml pergamum-html.xsl db/books.xml book isbn
+java Style  content/publisher-books.xml pergamum-html.xsl db/publishers.xml publisher id
+java Style  content/category-books.xml pergamum-html.xsl db/categories.xml category id
 
 for xml in comingsoon.xml listbooks.xml listcategories.xml listpublishers.xml newbooks.xml newreviews.xml help.xml
 do
-    java Style content/$xml pergamum.xsl
+    java Style content/$xml pergamum-html.xsl
 done
 
-java Style content/index.xml pergamum.xsl
+java Style content/index.xml pergamum-html.xsl
