c7e02c6c2e2c1ca7a7795b9a57acdf9d780554b4
rzigweid <rzigweid@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2004-04-19 00:48:04 +0000

    Fixed Some stupid mistakes.  Changed $LastChangedRevision$ to $Rev$ because I continally mistype the former.

--- osjava-threads/src/java/org/osjava/threads/ThreadContext.java
+++ osjava-threads/src/java/org/osjava/threads/ThreadContext.java
@@ -1,26 +1,39 @@
 /*
- * org.osjava.threads.ThreadContext $Id$ $LastChangedRevision$ $LastChangedDate$
- * $LastChangedBy$ $Author$ Created on Mar 24, 2004 Copyright (c) 2004, Robert
- * M. Zigweid All rights reserved. Redistribution and use in source and binary
- * forms, with or without modification, are permitted provided that the
- * following conditions are met: + Redistributions of source code must retain
- * the above copyright notice, this list of conditions and the following
- * disclaimer. + Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution. +
- * Neither the name of the TigThreads nor the names of its contributors may be
- * used to endorse or promote products derived from this software without
- * specific prior written permission. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
- * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
- * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * org.osjava.threads.ThreadContext
+ * $Id$
+ * $Rev$ 
+ * $LastChangedDate$ 
+ * $Author$ 
+ * 
+ * Created on Mar 24, 2004
+ *
+ *  Copyright (c) 2004, Robert M. Zigweid All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * + Redistributions of source code must retain the above copyright notice, 
+ *   this list of conditions and the following disclaimer. 
+ * 
+ * + Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation 
+ *   and/or other materials provided with the distribution. 
+ * 
+ * + Neither the name of the TigThreads nor the names of its contributors may
+ *   be used to endorse or promote products derived from this software without 
+ *   specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE. 
  */
 
 package org.osjava.threads;
@@ -40,7 +53,7 @@ import javax.naming.NamingException;
  * A Context for managing Threads and ThreadGroups.
  * 
  * @author Robert M. Zigweid
- * @version $LastChangedRevision $ $LastChangedDate$
+ * @version $Rev$ $LastChangedDate$
  * @since OSJava Threads 2.0
  */
 public class ThreadContext
