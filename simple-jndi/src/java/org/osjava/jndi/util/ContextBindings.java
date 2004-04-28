e6745b94e04128d060b76d63156ed565f68c630f
rzigweid <rzigweid@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2004-04-28 13:32:30 +0000

    Fixed a mistake in the license of ContextBindings and ContextNames.
    Fixed PropertiesContext to actually use ContextNames.  I am teh sux0r

--- simple-jndi/src/java/org/osjava/jndi/util/ContextBindings.java
+++ simple-jndi/src/java/org/osjava/jndi/util/ContextBindings.java
@@ -20,7 +20,7 @@
  *   this list of conditions and the following disclaimer in the documentation 
  *   and/or other materials provided with the distribution. 
  * 
- * + Neither the name of the TigThreads nor the bindings of its contributors may
+ * + Neither the name of the Simple-JNDI nor the bindings of its contributors may
  *   be used to endorse or promote products derived from this software without 
  *   specific prior written permission.
  * 
