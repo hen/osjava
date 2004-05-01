227e30bb2634f98e46967fcc3dc00b8e632553f5
hen <hen@users.noreply.github.com>
2004-05-01 18:53:03 +0000

    added javadoc

--- simple-jndi/src/java/org/osjava/jndi/PropertiesFactory.java
+++ simple-jndi/src/java/org/osjava/jndi/PropertiesFactory.java
@@ -36,6 +36,9 @@ import javax.naming.spi.InitialContextFactory;
 import javax.naming.Context;
 import java.util.Hashtable;
 
+/**
+ * Very simple factory for a PropertiesContext. 
+ */
 public class PropertiesFactory implements InitialContextFactory {
 
     public PropertiesFactory() {
