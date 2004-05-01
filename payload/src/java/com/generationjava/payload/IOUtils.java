2803ff55d8fedc1c0d070f289acecbc63dc2c293
hen <hen@users.noreply.github.com>
2004-05-01 19:18:43 +0000

    fixed imports, added javadoc

--- payload/src/java/com/generationjava/payload/IOUtils.java
+++ payload/src/java/com/generationjava/payload/IOUtils.java
@@ -31,10 +31,15 @@
  */
 package com.generationjava.payload;
 
-import java.util.*;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.OutputStream;
 
-import java.io.*;
-
+/**
+ * Some helper methods to help with Payload's IO work. 
+ */
 public class IOUtils {
 
     static void pushBytes(InputStream in, OutputStream out) throws IOException {
