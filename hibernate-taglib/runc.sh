077acf20f282d920dc06ef675987cfee513aa86d
hen <hen@users.noreply.github.com>
2004-05-02 19:32:58 +0000

    updated the compile script

--- hibernate-taglib/runc.sh
+++ hibernate-taglib/runc.sh
@@ -1,2 +1,2 @@
-javac -classpath .:servlet-api.jar:hibernate2.jar:jsp-api.jar org/osjava/hibernate/taglib/*.java 
+javac -classpath .:commons-beanutils.jar:servlet-api.jar:hibernate2.jar:jsp-api.jar org/osjava/hibernate/taglib/*.java 
 jar cf hibernate-taglib.jar org/osjava/hibernate/taglib/*.class META-INF/taglib.tld
