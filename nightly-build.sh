c16e7d9d2c754296aa09a00a7b3e0b13ba45576d
hen <hen@users.noreply.github.com>
2004-05-03 06:07:03 +0000

    a nightly build system. nothing fancy yet.

---
+++ nightly-build.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+# Big nasty rm that blows away quietly
+function silentrm() {
+    for file in $*
+    do
+        if [ -f $file ];
+        then
+            rm $file
+        fi
+        if [ -d $file ];
+        then
+            rm -r $file
+        fi
+    done
+}
+
+silentrm report/
+mkdir report/
+reportDir=`pwd`/report
+
+for i in `cat NIGHTLY.txt | grep -v '^#' `
+do
+#F#    failed=0
+    echo "Building $i"
+    mkdir -p report/$i
+    cd $i
+    # cleaning
+    silentrm target/ maven.log velocity.log ERROR.log OUTPUT.log
+    # building
+    maven -b jar 2> ERROR.log > OUTPUT.log
+    if [ $? -ne 0 ];
+    then
+        # create failed report
+        echo "Failed to build $i"
+        echo "Failed to build $i" > $reportDir/$i/FAILED
+        mv ERROR.log OUTPUT.log $reportDir/$i
+#F#        failed=1
+    fi
+    if [ -e ERROR.log ];
+    then
+        count=`cat ERROR.log | grep -v 'deprecat' | wc -l | sed 's/ //g'`
+        if [ $count != '0' ];
+        then
+            # errors in report
+            echo "There were errors in building $i"
+            echo "There were errors in building $i" > $reportDir/$i/FAILED
+            mv ERROR.log OUTPUT.log $reportDir/$i
+#F#            failed=1
+        fi
+    fi
+    # run checkstyle, pmd, junit report, javadoc
+#F#    if [ $failed -eq 0 ];
+#F#    then
+        echo "Building documentation"
+        maven -b checkstyle maven-junit-report-plugin:report pmd javadoc 2> DOCS-ERROR.log > DOCS-OUTPUT.log
+#F#    fi
+    
+    # somehow create a site from these reports
+    if [ -d target/docs/apidocs/ ];
+    then
+        mv target/docs/apidocs/ $reportDir/$i/javadoc
+    fi
+    if [ -f target/generated-xdocs/pmd-report.xml ];
+    then
+        mv target/generated-xdocs/pmd-report.xml $reportDir/$i
+    fi
+    if [ -f target/generated-xdocs/checkstyle-report.xml ];
+    then
+        mv target/generated-xdocs/checkstyle-report.xml $reportDir/$i
+    fi
+    if [ -f target/generated-xdocs/junit-report.xml ];
+    then
+        mv target/generated-xdocs/junit-report.xml $reportDir/$i
+    fi
+
+    echo "Finished with $i"
+    cd -
+done
---
+++ nightly-report.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+reportDir=`pwd`/report
+
+index=$reportDir/index.html
+
+echo '<html><body>' > $index
+echo '<h1>OSJava Nightly build</h1>' >> $index
+
+echo '<ul>' >> $index
+
+for i in `cat NIGHTLY.txt | grep -v '^#' `
+do
+
+    # now we create a pretty site
+    cd $reportDir/$i
+    local_report=report_`echo $i | sed 's/\//_/g'`.html
+    report=$reportDir/$local_report
+
+    cat pmd-report.xml | sed 's/document>/html>/' | sed 's/properties>/head>/' | sed 's/<\/section>//' | sed 's/<\/subsection>//' | sed 's/<section name="\([^"]*\)"/<h1>\1<\/h1/' | sed 's/<subsection name="\([^"]*\)"/<h2>\1<\/h2/' > pmd-report.html
+    cat checkstyle-report.xml | sed 's/document>/html>/' | sed 's/properties>/head>/' | sed 's/<\/section>//' | sed 's/<\/subsection>//' | sed 's/<section name="\([^"]*\)"/<h1>\1<\/h1/' | sed 's/<subsection name="\([^"]*\)"/<h2>\1<\/h2/' > checkstyle-report.html
+    cat junit-report.xml | sed 's/document>/html>/' | sed 's/properties>/head>/' | sed 's/<\/section>//' | sed 's/<\/subsection>//' | sed 's/<section name="\([^"]*\)"/<h1>\1<\/h1/' | sed 's/<subsection name="\([^"]*\)"/<h2>\1<\/h2/' > junit-report.html
+
+    echo '<html><body>' >> $report
+    echo "<h1>$i Report</h1>" >> $report
+
+    echo '<ul>' >> $report
+    echo "<li><a href='$i/checkstyle-report.html'>checkstyle</a></li>" >> $report
+    echo "<li><a href='$i/pmd-report.html'>pmd</a></li>" >> $report
+    echo "<li><a href='$i/junit-report.html'>junit</a></li>" >> $report
+    echo "<li><a href='$i/javadoc/'>javadoc</a></li>" >> $report
+    echo '</ul>' >> $report
+
+    echo '<hr>' >> $report
+
+    if [ -f FAILED ];
+    then
+        echo '<pre>' `cat FAILED` '</pre>' >> $report
+        echo '<hr>' >> $report
+        echo '<pre>' `cat ERROR.log | sed 's/$/<br>/'` '</pre>' >> $report
+
+        echo "<li><font color='#FF0000'>failed</font>&nbsp;&nbsp;- <a href='$local_report'>$i</a></li>" >> $index
+    else
+        echo '<pre>Build successful.</pre>' >> $report
+
+        echo "<li><font color='#00FF00'>success</font>&nbsp;- <a href='$local_report'>$i</a></li>" >> $index
+    fi
+    echo '</body></html>' >> $report
+
+    cd -
+done
+
+echo '</ul>' >> $index
+echo '</html></body>' >> $index
