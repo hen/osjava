e473e94847fa3f3454df4a7f699f30b65ca6d95f
hen <hen@users.noreply.github.com>
2004-05-13 05:10:39 +0000

    added licence and javadoc

--- genjava/gj-find/src/java/com/generationjava/util/WildcardUtils.java
+++ genjava/gj-find/src/java/com/generationjava/util/WildcardUtils.java
@@ -1,9 +1,48 @@
+/*
+ * Copyright (c) 2003-2004, Henri Yandell
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or 
+ * without modification, are permitted provided that the 
+ * following conditions are met:
+ * 
+ * + Redistributions of source code must retain the above copyright notice, 
+ *   this list of conditions and the following disclaimer.
+ * 
+ * + Redistributions in binary form must reproduce the above copyright notice, 
+ *   this list of conditions and the following disclaimer in the documentation 
+ *   and/or other materials provided with the distribution.
+ * 
+ * + Neither the name of Genjava-Core nor the names of its contributors 
+ *   may be used to endorse or promote products derived from this software 
+ *   without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.generationjava.util;
 
 import java.util.ArrayList;
 
+/**
+ * An implementation of Wildcard logic, as seen on command lines 
+ * on UNIX and DOS.
+ */
 public class WildcardUtils {
 
+    /**
+     * See if a particular piece of text, often a filename, 
+     * matches to a specified wildcard. 
+     */
     public static boolean match(String text, String wildcard) {
         // split wildcard on ? and *
         // for each element of the array, find a matching block in text
@@ -44,6 +83,7 @@ public class WildcardUtils {
         return true;
     }
 
+    // package level so a unit test may run on this
     static String[] splitOnTokens(String text) {
         char[] array = text.toCharArray();
         if(text.indexOf("?") == -1 && text.indexOf("*") == -1) {
