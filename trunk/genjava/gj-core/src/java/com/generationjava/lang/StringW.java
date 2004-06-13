aaae946a92f749a2f04921eeff78d7e83583c2bd
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2004-06-13 03:42:14 +0000

    removed levenstein as it is in commons-lang

--- trunk/genjava/gj-core/src/java/com/generationjava/lang/StringW.java
+++ trunk/genjava/gj-core/src/java/com/generationjava/lang/StringW.java
@@ -53,69 +53,6 @@ final public class StringW {
 	}
 
     /**
-     * Find the Levenshtein distance between two strings.
-     * This is the number of changes needed to change one string into 
-     * another. Where each change is a single character modification.
-     *
-     * This implemmentation of the levenshtein distance algorithm 
-     * is from http://www.merriampark.com/ld.htm
-     */
-    static public int getLevenshteinDistance(String s, String t) {
-        int d[][]; // matrix
-        int n; // length of s
-        int m; // length of t
-        int i; // iterates through s
-        int j; // iterates through t
-        char s_i; // ith character of s
-        char t_j; // jth character of t
-        int cost; // cost
-
-        // Step 1
-        n = s.length ();
-        m = t.length ();
-        if (n == 0) {
-            return m;
-        }
-        if (m == 0) {
-            return n;
-        }
-        d = new int[n+1][m+1];
-
-        // Step 2
-        for (i = 0; i <= n; i++) {
-            d[i][0] = i;
-        }
-
-        for (j = 0; j <= m; j++) {
-            d[0][j] = j;
-        }
-
-        // Step 3
-        for (i = 1; i <= n; i++) {
-            s_i = s.charAt (i - 1);
-
-            // Step 4
-            for (j = 1; j <= m; j++) {
-                t_j = t.charAt (j - 1);
-
-                // Step 5
-                if (s_i == t_j) {
-                    cost = 0;
-                } else {
-                    cost = 1;
-                }
-
-                // Step 6
-                d[i][j] = NumberUtils.minimum(d[i-1][j]+1, d[i][j-1]+1, d[i-1][j-1] + cost);
-            }
-        }
-
-        // Step 7
-        return d[n][m];
-    }
-
-
-    /**
      * Create a word-wrapped version of a String. Wrap at 80 characters and 
      * use newlines as the delimiter. If a word is over 80 characters long 
      * use a - sign to split it.
