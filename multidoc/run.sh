0ad9a9a4ea0be3ce5b404927c7bafaf5f27ec0d7
hen <hen@users.noreply.github.com>
2004-07-11 04:31:42 +0000

    rather hacky multidoc system. adding as I am about to start rewriting a bunch and want to be able to rollback

---
+++ multidoc/run.sh
@@ -0,0 +1,2 @@
+java -classpath /Users/hen/osj/trunk/genjava/gj-scrape/target/gj-scrape-2.0.1.jar:/Users/hen/.maven/repository/genjava/jars/gj-core-3.0.jar:.:/Users/hen/.maven/repository/commons-lang/jars/commons-lang-2.0.jar org.osjava.multidoc.Multidoc $*
+cp docs/* $2
