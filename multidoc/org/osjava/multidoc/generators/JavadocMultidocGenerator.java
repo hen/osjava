666253eb2c05ffd1ae8b77c67f68ebda566c529d
hen <hen@users.noreply.github.com>
2004-07-12 21:34:33 +0000

    help is now about and is dynamic. also a dynamic front page and the configuration is lamely copied in

--- multidoc/org/osjava/multidoc/generators/JavadocMultidocGenerator.java
+++ multidoc/org/osjava/multidoc/generators/JavadocMultidocGenerator.java
@@ -25,7 +25,7 @@ public class JavadocMultidocGenerator implements MultidocGenerator {
     public void writeProjectFrame(Writer writer, DocumentSite site, Document document) throws IOException {
         writer.write("<html><head><LINK REL ='stylesheet' TYPE='text/css' HREF='");
         writer.write(site.getStylesheet());
-        writer.write("' TITLE='Style'><script src='../multidoc.js'/></head><body>\n");
+        writer.write("' TITLE='Style'><script src='../multidoc.js'></script></head><body>\n");
 //        writer.write("<table width='100%'><tr><td class='NavBarCell1'><a href='overview-summary.html' target='classFrame'><FONT CLASS='NavBarFont1'><b>Overview</b></FONT></a>\n");
 //        writer.write("<a href=''><FONT CLASS='NavBarFont1'><b>Index</a></b></FONT></a>\n");
 //        writer.write("<a href='help-doc.html' target='classFrame'><FONT CLASS='NavBarFont1'><b>Help</b></FONT></a></td></tr></table>\n");
@@ -69,7 +69,7 @@ public class JavadocMultidocGenerator implements MultidocGenerator {
         writer.write("<HTML><HEAD><TITLE>MetaOverview (Jakarta)</TITLE>\n");
         writer.write("<LINK REL ='stylesheet' TYPE='text/css' HREF='");
         writer.write(site.getStylesheet());
-        writer.write("' TITLE='Style'><script src='multidoc.js'/>\n");
+        writer.write("' TITLE='Style'><script src='../multidoc.js'></script>\n");
         writer.write("</HEAD>\n");
         writer.write("<BODY BGCOLOR='white'>\n");
 //        writer.write("<FONT CLASS='FrameItemFont'><A HREF='allclasses-frame.html' target='packageFrame'>All Classes</A></FONT>\n");
@@ -102,7 +102,7 @@ public class JavadocMultidocGenerator implements MultidocGenerator {
         writer.write("<HTML><HEAD><TITLE>Overview (Multidoc)</TITLE>\n");
         writer.write("<LINK REL ='stylesheet' TYPE='text/css' HREF='");
         writer.write(site.getStylesheet());
-        writer.write("' TITLE='Style'><script src='multidoc.js'/>\n");
+        writer.write("' TITLE='Style'><script src='../multidoc.js'></script>\n");
         writer.write("<SCRIPT type='text/javascript'>\n");
         writer.write("function windowTitle() {\n");
         writer.write("    parent.document.title='Overview (Multidoc)';\n");
