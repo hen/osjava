09788a3d3dc488abf0fb6e649be5d4b845917848
hen <hen@users.noreply.github.com>
2004-07-22 02:34:28 +0000

    new perl version

--- integration/integrate-builds.pl
+++ integration/integrate-builds.pl
@@ -1,7 +1,12 @@
 #!/usr/bin/perl -w
 
+use Cwd;
+use File::Path;
 use XML::Simple;
 use Data::Dumper;
+use Time::Interval;
+use File::Copy;
+use Template;
 
 sub usage() {
     print "Usage:\n";
@@ -16,12 +21,13 @@ sub checkout {
     my ($repository, $project, $tag, $directory)  = @_;
     if($repository =~ /svn:/) {
         if($tag) {
+            # also change the repository over to releases
             $tag = '-'.$tag;
         } else {
             $tag = '';
         }
         $repository = substr( $repository, 4 );
-        system "svn co $repository/$project-$tag $directory";
+        system "svn co $repository/$project $directory";
     } 
     elsif($repository =~ /cvs:/) {
         if($tag) {
@@ -38,10 +44,11 @@ sub checkout {
 
 sub update {
     my ($repository, $directory)  = @_;
+    $previousDirectory = getcwd();
     chdir $directory;
     if($repository =~ /svn:/) {
         $repository = substr( $repository, 4 );
-        system "svn update";
+        $update_info = `svn update`;
     } 
     elsif($repository =~ /cvs:/) {
         $repository = substr( $repository, 3 );
@@ -49,10 +56,25 @@ sub update {
     } else {
         warn "Attempt to use unknown type of repository. \n";
     }
-    chdir '..';
+    chdir $previousDirectory;
     return $update_info;
 }
 # end of repository methods
+
+sub xdoc2html {
+    my ($source, $target)  = @_;
+    open(SOURCE, $source);
+    open(TARGET, ">$target");
+    while(<SOURCE>) {
+       s/document\>/html\>/;
+       s/properties\>/head\>/;
+       s/\<\/section\>//;
+       s/\<\/subsection\>//;
+       s/\<section name="([^"]*)"/\<h1\>$1\<\/h1/;
+       s/\<subsection name="([^"]*)"/\<h2\>$1\<\/h2/;
+       print TARGET $_;
+    }
+}
 
 if(@ARGV < 2) {
     usage();
@@ -68,162 +90,133 @@ my $conf = XMLin($conf_filename, forcearray=>1);
 #NOT YET#$global_repository = $conf->{'buildables'}->{'repository'};
 @projects = @{$conf->{'buildables'}[0]->{'buildable'}};
 
-##print Dumper($conf);
+##print Dumper($conf->{'site'}[0]);
 
 my @build_list = ();
 
 # action may be 'all', 'update' or 'project-name' 
 
 # loop over each component in the xml
-foreach $buildable (@projects) {
-    $project = $buildable->{'project'};
-    $repository = $buildable->{'repository'};
-    $tag = $buildable->{'tag'};
-    if(not $tag) {
-        $directory = $project;
-    } else {
-        $directory = ${project}. '_' . $tag;
-    }
+    foreach $buildable (@projects) {
+        $project = $buildable->{'project'};
+        $repository = $buildable->{'repository'};
+        $tag = $buildable->{'tag'};
+        if(not $tag) {
+            $directory = $project;
+        } else {
+            $directory = ${project}. '_' . $tag;
+        }
+        $buildable->{'directory'} = $directory;
+        $buildable->{'escapedDirectory'} = $directory;
+        $buildable->{'escapedDirectory'} =~ s/\//_/g;
 
-    # is component there?
-    if(-e $directory) {
-        # yes: do update check. 
-        $updated = update($repository, $directory);
+        # is component there?
+        if(-e $directory) {
+            # yes: do update check. 
+            print "Updating $directory\n";
+            $updated = update($repository, $directory);
 
-        # if all:
-        if($action eq 'all') {
+            # if all:
+            if($action eq 'all') {
+                # put in list of ones to build - build-list
+                push @build_list, $buildable;
+            }
+            elsif($updated) {
+                # put in list of ones to build - build-list
+                push @build_list, $buildable;
+            }
+        } else {
+            print "Checking out $directory\n";
+            # no: checkout
+            checkout($repository, $project, $tag, $directory);
             # put in list of ones to build - build-list
             push @build_list, $buildable;
         }
-        elsif($updated) {
-            # put in list of ones to build - build-list
-            push @build_list, $buildable;
-        }
-    } else {
-        # no: checkout
-        checkout($repository, $project, $tag, $directory);
-        # put in list of ones to build - build-list
-        push @build_list, $buildable;
     }
-}
-}
 
+
+# check if these are really needed
+    unlink('LAST_BUILD');
+    unlink('SCM_UPDATE');
+    unlink('REASON');
+    mkpath('report');
 
     # loop over each component in build-list
     foreach $buildable (@build_list) {
-        # install a snapshot jar
-        # build the main jar - record build-time
-        # if failed(), add to build-report
-        # build the site - maven + ttk
-        # deploy deployables
-        # report build-time, build-date, update-reason
-        print $buildable->{'project'}."\n";
-    }
+        if( not -e $buildable->{'directory'} ) {
+            warn "Unable to build $buildable->{'project'} as not checked out. ";
+            chdir $previousDirectory;
+            next;
+        }
+        $previousDirectory = getcwd();
+        chdir $buildable->{'directory'};
+        if( not -e 'project.xml' ) {
+            warn "Unable to build $buildable->{'project'} as not mavenised. ";
+            chdir $previousDirectory;
+            next;
+        }
 
-#rm -f LAST_BUILD SCM_UPDATE REASON
-#buildDir=`pwd`
-#
-#if [ ! -d report/ ]; 
-#then
-#    mkdir report/
-#fi
-#reportDir=`pwd`/report
-#
-#for i in $LIST
-#do
-#    if [ ! -e $i/project.xml ];
-#    then
-#        echo "Unable to build $i, as not mavenised. "
-#        continue
-#    fi
-#    rm -fr report/$i
-#    echo "Building $i"
-#    echo $i >> LAST_BUILD
-#    mkdir -p report/$i
-#    cd $i
-#    # cleaning
-#    rm -fr target/ maven.log velocity.log ERROR.log OUTPUT.log
-#    # building
-#    TIME_START=`date +%s`
-#    maven -b clean jar:install-snapshot clean jar 2> ERROR.log > OUTPUT.log
-#    maven -b clean jar 2> ERROR.log > OUTPUT.log
-#    TIME_END=`date +%s`
-#    BUILD_DURATION=`echo $TIME_END - $TIME_START | bc`
-#    BUILD_DURATION=`date -d "1970-01-01 UTC $BUILD_DURATION seconds" +"%M minutes %S seconds"`
-#    if [ $? -ne 0 ];
-#    then
-#        # create failed report
-#        echo "Failed to build $i"
-#        echo "Failed to build $i" > $reportDir/$i/FAILED
-#        mv ERROR.log OUTPUT.log $reportDir/$i
-#        if [ $INIT = 'update' ];
-#        then
+        unlink 'maven.log', 'velocity.log', 'ERROR.log', 'OUTPUT.log', 'DOCS-ERROR.log', 'DOCS-OUTPUT.log';
+        print "Building $buildable->{'project'}\n";
+
+# Build the code
+        # install a snapshot jar
+        system("maven -b clean jar:install-snapshot clean jar 2> /dev/null > /dev/null");
+        # build the main jar - record build-time
+        my $start = time();
+        system("maven -b clean jar 2> ERROR.log > OUTPUT.log");
+        my $end = time();
+#        my $duration = getInterval($start, $end, "string");
+
+        # see if it was a failure
+        my $failure = $? >> 8;
+        if(not $failure) {
+            if(-e 'ERROR.log') {
+                # search the error log to see if it contains certain phrases
+                open(ERROR_FILE, 'ERROR.log');
+                while(<ERROR_FILE>) {
+                    if( /BUILD_FAILED|build cannot continue/ ) {
+# seems to then grep for [ERROR] in OUTPUT.log and put this in ERROR.log
+                        $failure = 1; 
+                    }
+                }
+                close(ERROR_FILE);
+            }
+        }
+        if($failure) {
+            print "Failed to build $buildable->{'project'}\n";
+            if($action eq 'update') {
+                print "Need to mail out a failure. \n";
 #            cat $reportDir/$i/ERROR.log | mail -s "Failed to build $i" bayard@osjava.org
-#        fi
-#    fi
-#    if [ -e ERROR.log ];
-#    then
-#        count=`cat ERROR.log | grep 'BUILD FAILED\|build cannot continue' | wc -l | sed 's/ //g'`
-#        if [ $count != '0' ];
-#        then
-#            # errors in report
-#            echo "There were errors in building $i"
-#            echo "There were errors in building $i" > $reportDir/$i/FAILED
-#            if [ $count != '0' ];
-#            then
-#                echo >> ERROR.log
-#                cat OUTPUT.log | grep '\[ERROR\] TEST' >> ERROR.log
-#            fi
-#            mv ERROR.log OUTPUT.log $reportDir/$i
-#            if [ $INIT = 'update' ];
-#            then
-#                cat $reportDir/$i/ERROR.log | mail -s "Failed to build $i" bayard@osjava.org
-#            fi
-#        fi
-#    fi
-#    # run checkstyle, pmd, junit report, javadoc, xref
-#    echo "Building documentation"
-#    maven -b maven-simian-plugin maven-jdepend-plugin:report maven-jcoverage-plugin:report checkstyle maven-junit-report-plugin:report maven-jxr-plugin:report pmd javadoc 2> DOCS-ERROR.log > DOCS-OUTPUT.log
-#    
-#    # somehow create a site from these reports
-#    if [ -d target/docs/apidocs/ ];
-#    then
-#        mv target/docs/apidocs/ $reportDir/$i/apidocs
-#    fi
-#    if [ -d target/docs/jcoverage/ ];
-#    then
-#        mv target/docs/jcoverage/ $reportDir/$i/jcoverage
-#    fi
-#    if [ -f target/generated-xdocs/simian-report.xml ];
-#    then
-#        mv target/generated-xdocs/simian-report.xml $reportDir/$i
-#    fi
-#    if [ -f target/generated-xdocs/jdepend-report.xml ];
-#    then
-#        mv target/generated-xdocs/jdepend-report.xml $reportDir/$i
-#    fi
-#    if [ -f target/generated-xdocs/pmd-report.xml ];
-#    then
-#        mv target/generated-xdocs/pmd-report.xml $reportDir/$i
-#    fi
-#    if [ -d target/docs/xref/ ];
-#    then
-#        mv target/docs/xref $reportDir/$i
-#    fi
-#    if [ -d target/docs/xref-test/ ];
-#    then
-#        mv target/docs/xref-test $reportDir/$i
-#    fi
-#    if [ -f target/generated-xdocs/checkstyle-report.xml ];
-#    then
-#        mv target/generated-xdocs/checkstyle-report.xml $reportDir/$i
-#    fi
-#    if [ -f target/generated-xdocs/junit-report.xml ];
-#    then
-#        mv target/generated-xdocs/junit-report.xml $reportDir/$i
-#    fi
-#
-## Needs to get smarter...
+            }
+            $buildable->{'failed'} = "FAILED";
+        }
+
+
+
+# build the site - maven + ttk
+        print "Building documentation\n";
+        system("maven -b maven-simian-plugin maven-jdepend-plugin:report maven-jcoverage-plugin:report checkstyle maven-junit-report-plugin:report maven-jxr-plugin:report pmd javadoc 2> DOCS-ERROR.log > DOCS-OUTPUT.log");
+        # report build-time, build-date, update-reason
+
+        my $reportDir = "${previousDirectory}/report/$buildable->{'directory'}";
+        rmtree($reportDir);
+        mkpath($reportDir);
+
+        # deploy reports
+        move "target/docs/apidocs/", "$reportDir/apidocs/";
+        move "target/docs/jcoverage/", "$reportDir/jcoverage/";
+        move "target/docs/xref/", "$reportDir/xref/";
+        move "target/docs/xref-test/", "$reportDir/xref-test/";
+        move "target/generated-xdocs/simian-report.xml", $reportDir;
+        move "target/generated-xdocs/pmd-report.xml", $reportDir;
+        move "target/generated-xdocs/jdepend-report.xml", $reportDir;
+        move "target/generated-xdocs/checkstyle-report.xml", $reportDir;
+        move "target/generated-xdocs/junit-report.xml", $reportDir;
+
+        # deploy targets
+
+## Needs to get smarter.
 #    DEPLOYABLES=`ls -1 target/*.?ar 2>/dev/null`
 ##    rm -f $reportDir/$i/builds/*.jar
 #    DT=`date +%Y%m%d`
@@ -249,3 +242,56 @@ foreach $buildable (@projects) {
 #    echo "Finished with $i"
 #    cd -
 #done
+
+        chdir $previousDirectory;
+    }
+
+$timestamp = time();
+
+
+# REPORT SYSTEM
+    print "Building front page\n";
+    my $tt = Template->new();
+    my $input = 'templates/frontpage.tt';
+    my $vars = {
+        projects => \@projects,
+        site => $conf->{'site'}[0],
+        timestamp => $timestamp,
+    };
+    $tt->process($input, $vars, "report/index.html") || die $tt->error();
+
+    foreach $buildable (@build_list) {
+        print "Building report for $buildable->{'project'}\n";
+
+        $directory = "report/$buildable->{'directory'}";
+  
+        # create report pages from xml files
+        my %reports;
+        my @xml_reports = ('checkstyle', 'pmd', 'junit', 'simian', 'jdepend');
+  
+        foreach $xml_report (@xml_reports) {
+            if( -e "$directory/${xml_report}-report.xml" ) {
+                xdoc2html("$directory/${xml_report}-report.xml", "$directory/${xml_report}-report.html");
+                $reports{$xml_report} = "$directory/${xml_report}-report.html";
+            }
+        }
+  
+        my %sub_sites = ('apidocs' => 'javadoc', 'xref' => 'source', 'jcoverage' => 'test coverage', 'builds' => 'builds');
+        while( ($key, $value) = each %sub_sites) {
+            if(-e "$directory/$key") {
+                $reports{$key} = "$buildable->{'directory'}/$value";
+            }
+        }
+  
+        # generate a report page, with links if targets of said links are available
+        # TODO: Make the links unactive if not available, rather than not there
+        my $input = 'templates/project.tt';
+        my $vars = {
+            project => $buildable,
+            site => $conf->{'site'}[0],
+            timestamp => $timestamp,
+            reports => \%reports,
+            build_reason => $action,
+        };
+        $tt->process($input, $vars, "report/report_".$buildable->{'escapedDirectory'}.".html") || die $tt->error();
+    }
