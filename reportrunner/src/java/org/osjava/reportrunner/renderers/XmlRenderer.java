10a1a3ff86f32b46bb01f763a691de16b1617eb2
hen <hen@users.noreply.github.com>
2004-10-18 16:40:58 +0000

    xml renderer implemented: #RRR-4

---
+++ reportrunner/src/java/org/osjava/reportrunner/renderers/XmlRenderer.java
@@ -0,0 +1,37 @@
+package org.osjava.reportrunner.renderers;
+
+import java.io.*;
+import java.util.*;
+import com.generationjava.io.xml.*;
+
+import org.osjava.reportrunner.*;
+
+public class XmlRenderer extends AbstractRenderer {
+
+    public void display(Result result, Report report, Writer out) throws IOException {
+        PrettyPrinterXmlWriter writer = new PrettyPrinterXmlWriter(new SimpleXmlWriter(out));
+        writer.setIndent("  ");
+        writer.setNewline("\n");
+
+        boolean numbers = false;
+
+        writer.writeEntity(report.getName());
+
+        Column[] columns = report.getColumns();
+        if(columns.length == 0) {
+            numbers = true;
+        }
+
+        while(result.hasNextRow()) {
+            writer.writeEntity("row");
+            Object[] row = result.nextRow();
+            for(int j=0; j<row.length; j++) {
+                writer.writeAttribute( numbers?"field"+j:columns[j].getName(), row[j] );
+            }
+            writer.endEntity();
+        }
+
+        writer.endEntity();
+    }
+
+}
