6c4501e9e23e6b2e699af9d29c7c56b8f5c7d16b
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2004-11-15 18:18:14 +0000

    Resource idea partly added; Factory cleaned up a touch with the ignore idea

---
+++ trunk/reportrunner/src/java/org/osjava/reportrunner/Resource.java
@@ -0,0 +1,21 @@
+package org.osjava.reportrunner;
+
+public class Resource {
+
+    private String name;
+    private String label;
+    private String uri;
+    private String type;
+
+    public String getName() { return this.name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getLabel() { return this.label; }
+    public void setLabel(String label) { this.label = label; }
+
+    public String getUri() { return this.uri; }
+    public void setUri(String uri) { this.uri = uri; }
+
+    public String getType() { return this.type; }
+    public void setType(String type) { this.type = type; }
+}
