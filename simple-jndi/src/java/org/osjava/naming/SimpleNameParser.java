4968d9e6dac44be29ee2c93992a50bb21e9df815
rzigweid <rzigweid@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2004-11-20 21:36:01 +0000

    Added a 'SimpleNameParser' object.  It's not done yet, and is really copied from OSJava-Threads ThreadNameParser (soon to disappear and use this one).  It is, however, a start, and Simple-JNDI appears to be in desparate need of one.
    More pissing on the fire hydrant to continue later.

---
+++ simple-jndi/src/java/org/osjava/naming/SimpleNameParser.java
@@ -0,0 +1,112 @@
+/*
+ * org.osjava.threads.ThreadNameParser
+ *
+ * $URL$
+ * $Id$
+ * $Rev$
+ * $Date$
+ * $Author$
+ *
+ * Created on Mar 24, 2004 by Robert M. Zigweid
+ */
+package org.osjava.naming;
+
+import java.util.Enumeration;
+import java.util.Properties;
+
+import javax.naming.CompoundName;
+import javax.naming.Context;
+import javax.naming.InvalidNameException;
+import javax.naming.Name;
+import javax.naming.NameParser;
+import javax.naming.NamingException;
+
+
+/**
+ * The NameParser for the ThreadContext.  
+ * 
+ * @author Robert M. Zigweid
+ * @version $LastChangedRevision $ $LastChangedDate$
+ * @since OSJava Threads 2.0
+ */
+public class SimpleNameParser implements NameParser {
+    
+    /*
+     * The parent Context.  This is necessary for aquiring relevant data, like 
+     * Properties that are used.
+     */
+    private Context parent = null;
+    
+    /*
+     * The properties utilized by the SimpleNameParser when constructing new
+     * names.
+     */
+    private Properties props = new Properties();
+    
+    /**
+     * Creates a ThreadNameParser.  Any relevant information that is needed, 
+     * such as the environment that is passed to {@link CompoundName CompundName}
+     * objects that are created.
+     * 
+     * @param parent ThreadContext that utilizes the name parser.
+     * @throws NamingException if a naming exception is found.
+     */
+    public SimpleNameParser(Context parent) throws NamingException {
+        this.parent = parent;
+        /* Properties from the parent context are in a HashTable. */
+        props.putAll(this.parent.getEnvironment());
+    }
+
+    /** 
+     * Parses a name into its components.<br/>
+     * (Copied from {@link javax.naming.NameParser#parse(java.lang.String)}
+     * 
+     * @param name The non-null string name to parse.
+     * @return A non-null parsed form of the name using the naming convention
+     *         of this parser.
+     * @throws InvalidNameException If the name does not conform to syntax 
+     *         defined for the namespace.
+     * @throws NamingException If a naming exception was encountered.
+     */
+    public Name parse(String name) 
+        throws InvalidNameException, NamingException {
+        Name ret = new CompoundName(name, props);
+        return ret;
+    }
+    
+    /**
+     * Determine whether or not <code>ob</code> is equal to this object.
+     * If the ob is an instance of ThreadNameParser, it is considered to be 
+     * equal.
+     * <br/><br/>
+     * <b>NOTE:</b> The above assumption may actually be false under two
+     * circomstances.   Firstly, if the properties utilized by the contexts
+     * are different.  Secondly, if the ThreadNameParser is subclassed.
+     * 
+     * @param ob the objct which is being compared to this one.
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    public boolean equals(Object ob) {
+        if(ob instanceof SimpleNameParser) {
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * Converts the <code>name</code> of type  {@link javax.naming.Name Name}
+     * to a String using the properties associated with the name parser.
+     * 
+     * @param name the name to be converted
+     * @return a string representation of <code>name</code>.
+     */
+    public String nameToString(Name name) {
+        StringBuffer retString = new StringBuffer("");
+        Enumeration parts = name.getAll();
+        while(parts.hasMoreElements()) {
+            String next = parts.nextElement().toString();
+            retString.append(next);
+        }
+        return retString.toString();
+    }
+}
