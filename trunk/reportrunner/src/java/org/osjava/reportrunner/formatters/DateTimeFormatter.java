7b4c59b99b6d7763b41a87e4199407268bce08f6
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2005-01-24 17:22:42 +0000

    hack to get around crappy Oracle Timestamp bug

--- trunk/reportrunner/src/java/org/osjava/reportrunner/formatters/DateTimeFormatter.java
+++ trunk/reportrunner/src/java/org/osjava/reportrunner/formatters/DateTimeFormatter.java
@@ -2,11 +2,27 @@ package org.osjava.reportrunner.formatters;
 
 import org.osjava.reportrunner.*;
 
+import java.lang.reflect.*;
 import java.text.*;
 
 public class DateTimeFormatter extends AbstractFormatter {
 
     public Object format(Object input) {
+        if("oracle.sql.TIMESTAMP".equals(input.getClass().getName())) {
+            try {
+                Method method = input.getClass().getMethod("timestampValue", new Class[0] );
+                input = method.invoke( input, new String[0] );
+            } catch(NoSuchMethodException nsme) {
+                nsme.printStackTrace();
+            } catch(IllegalAccessException iae) {
+                iae.printStackTrace();
+            } catch(IllegalArgumentException iae) {
+                iae.printStackTrace();
+            } catch(InvocationTargetException ite) {
+                ite.printStackTrace();
+            }
+        }
+
         if(input instanceof java.util.Date) {
             SimpleDateFormat sdf = new SimpleDateFormat( getPattern() );
             return sdf.format( (java.util.Date) input);
