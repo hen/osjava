e37a8e4f76bf9391f3905d8e68a0468478628cd7
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2005-01-27 16:19:59 +0000

    velocity renderer for template pages and jfreechart structure for use as images or applets

--- trunk/reportrunner/src/java/org/osjava/reportrunner_plugins/renderers/timechart/TimeChartRenderer.java
+++ trunk/reportrunner/src/java/org/osjava/reportrunner_plugins/renderers/timechart/TimeChartRenderer.java
@@ -13,16 +13,21 @@ public class TimeChartRenderer extends AbstractRenderer {
         PrettyPrinterXmlWriter writer = new PrettyPrinterXmlWriter(new SimpleXmlWriter(out));
         writer.writeEntity("html")
                .writeEntity("applet")
-                .writeAttribute("archive", "timechart/timechart.jar,timechart/jfreechart-0.9.20.jar,timechart/jcommon-0.9.5.jar,timechart/gj-xml-1.1.jar")
+                .writeAttribute("archive", "timechart/timechart.jar,timechart/jfreechart-0.9.21.jar,timechart/jcommon-0.9.6.jar,timechart/gj-xml-1.1.jar")
                 .writeAttribute("code", "org.osjava.reportrunner_plugins.renderers.timechart.TimeChartApplet.class")
                 .writeAttribute("width", "400")
-                .writeAttribute("height", "400")
-                .writeEntity("param")
-                 .writeAttribute("name", "timestamp-field")
-                 .writeAttribute("value", "timestamp")
-                .endEntity()
-               .endEntity()
-              .endEntity();
+                .writeAttribute("height", "400");
+        Map vars = getVariables();
+        Iterator keys = vars.keySet().iterator();
+        while(keys.hasNext()) {
+            Object key = keys.next();
+            writer.writeEntity("param")
+                 .writeAttribute("name", ""+key)
+                 .writeAttribute("value", ""+vars.get(key))
+                .endEntity();
+        }
+        writer.endEntity();
+        writer.endEntity();
         writer.close();
     }
 
