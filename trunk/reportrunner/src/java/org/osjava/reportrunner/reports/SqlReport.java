ec47b14f5834ddb72d5f48e8c0f04054ccc1b3a3
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2005-02-07 18:12:21 +0000

    implemented first stab at named sql parameters. the dbutils arrayhandler was broken out of being an inner class in SqlReport so it may easily be reused. part of #RRR-67

--- trunk/reportrunner/src/java/org/osjava/reportrunner/reports/SqlReport.java
+++ trunk/reportrunner/src/java/org/osjava/reportrunner/reports/SqlReport.java
@@ -50,31 +50,4 @@ public class SqlReport extends AbstractSqlReport {
         }
     }
 
-    private class ReportArrayListHandler extends ArrayListHandler {
-        
-        private Report report;
-
-        ReportArrayListHandler(Report report) {
-            this.report = report;
-        }
-
-        public Object handle(ResultSet rs) throws SQLException {
-            Object obj = super.handle(rs);
-            if(this.report.getColumns().length == 0) {
-                // use meta data to guess column names
-                ResultSetMetaData meta = rs.getMetaData();
-                int sz = meta.getColumnCount();
-                for(int i=1; i<=sz; i++) {
-                    Column column = new Column();
-                    column.setName(meta.getColumnName(i));
-                    column.setLabel(meta.getColumnLabel(i));
-                    this.report.addColumn(column);
-                }
-            }
-            return obj;
-        }
-
-    }
-
-
 }
