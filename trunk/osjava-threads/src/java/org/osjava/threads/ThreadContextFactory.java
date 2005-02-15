0f14610322244280cae7e32fcce747ea4145eb60
rzigweid <rzigweid@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2005-02-15 09:07:13 +0000

    * Renamed class  * Changed the default name to "", though I'm still not happy with this.  I    think it needs to be based upon some value in the environment.

---
+++ trunk/osjava-threads/src/java/org/osjava/threads/ThreadContextFactory.java
@@ -0,0 +1,71 @@
+/*
+ * org.osjava.threads.ThreadContextGenerator
+ * $Id$
+ * $Rev$ 
+ * $Date$ 
+ * $Author$
+ * $URL$
+ * 
+ * Created on Nov 28, 2004
+ *
+ * Copyright (c) 2004, Robert M. Zigweid All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * + Redistributions of source code must retain the above copyright notice, 
+ *   this list of conditions and the following disclaimer. 
+ *
+ * + Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation 
+ *   and/or other materials provided with the distribution. 
+ *
+ * + Neither the name of the OSJava-Threads nor the names of its contributors may
+ *   be used to endorse or promote products derived from this software without 
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+package org.osjava.threads;
+
+import java.util.Hashtable;
+
+import javax.naming.Context;
+import javax.naming.NamingException;
+import javax.naming.spi.InitialContextFactory;
+
+/**
+ * A simple InitialContextFactory to create an initial {@link ThreadContext} 
+ * from {@link javax.naming.InitialContext InitialContext}.
+ * 
+ * @author Robert M. Zigweid
+ * @since OSJava-Threads 2.0
+ */
+public class ThreadContextFactory implements InitialContextFactory {
+    /**
+     * Creates a new ThreadContextGenerator
+     */
+    public ThreadContextFactory() {
+        super();
+    }
+
+    /**
+     * @see javax.naming.spi.InitialContextFactory#getInitialContext(java.util.Hashtable)
+     */
+    public Context getInitialContext(Hashtable environment) throws NamingException {
+        return new ThreadContext(environment, "");
+    }
+
+}
