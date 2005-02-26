d2957f8d4eed22517185969d22f24a6480334fe5
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2005-02-26 01:08:18 +0000

    Latest version. Plays sounds, muted but nice colours.

--- trunk/pound/org/osjava/pound/Pound.java
+++ trunk/pound/org/osjava/pound/Pound.java
@@ -1,81 +1,254 @@
+/*
+ * Created on Feb 23, 2005
+ */
 package org.osjava.pound;
-
-import java.awt.*;
-import java.awt.event.*;
-
-// plan: use javax.sound to play musical notes on key input
-//       have screen change based on keys hit; velocity of keys hit; show letter on screen
-//       show animal matching the particular letter and play animal noise
+ 
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.HeadlessException;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+ 
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MetaEventListener;
+import javax.sound.midi.MetaMessage;
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Receiver;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
+import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Synthesizer;
+import javax.sound.midi.Track;
+import javax.sound.midi.Transmitter;
+ 
+/**
+ * @author hyandell
+ */
 public class Pound extends Frame implements KeyListener {
-
+ 
     public static void main(String[] args) {
         Pound p = new Pound();
         p.show();
     }
-
+ 
     private SimpleCanvas canvas;
-
-    public Pound() {
+ 
+    /**
+     * @throws java.awt.HeadlessException
+     */
+    public Pound() throws HeadlessException {
         super("Pound");
         this.addKeyListener(this);
-        canvas = new SimpleCanvas(Color.BLACK);
+        canvas = new SimpleCanvas();
+        canvas.addKeyListener(this);
         this.add(canvas);
         this.pack();
+        this.setSize(100, 200);
+       
     }
-
-
-    // KeyListener
-    public void keyTyped(KeyEvent ke) {
-        // ignore
-        ke.consume();
-    }
-
-    public void keyReleased(KeyEvent ke) {
-        // ignore
-        ke.consume();
-    }
-
+ 
     public void keyPressed(KeyEvent ke) {
         int keyCode = ke.getKeyCode();
-
-        if(keyCode == KeyEvent.VK_ESCAPE && ke.isShiftDown()) {
-            System.err.println("Quitting. ");
-            ke.consume();
+        int modifiers = ke.getModifiers();
+ 
+        if (keyCode == KeyEvent.VK_SHIFT) {
+            return;
+        }
+ 
+        if (keyCode == KeyEvent.VK_ESCAPE && ke.isShiftDown()) {
+            System.err.println("ESCAPE");
             this.dispose();
             System.exit(0);
         }
-
-        Color color = this.canvas.getBackground();
-        int r = color.getRed();
-        int g = color.getGreen();
-        int b = color.getBlue();
-        if(r != 255) r++;
-        if(g != 255) g++;
-        if(b != 255) b++;
-        this.canvas.setBackground(new Color(r,g,b));
-
-        System.err.println("Pressed: "+KeyEvent.getKeyText(keyCode)+" "+keyCode+" "+ke.getModifiers());
-        ke.consume();
+ 
+        System.err.println("Key: " + KeyEvent.getKeyText(keyCode) + " " + keyCode + " " + modifiers);
+       
+       
+        this.canvas.setBackground(chooseColor(this.canvas.getBackground(), keyCode)); //normalize(keyCode, 20, 110, 0, 255)));
+       
+        playNote(keyCode);//normalize(keyCode, 20, 110, 0, 127));
     }
-
+   
+    public int normalize(int keyCode, int min, int max, int new_min,
+int new_max) {
+        if(keyCode < min) keyCode = min;
+        if(keyCode < max) keyCode = max;
+        return new_min + (new_max - new_min) * (keyCode - min) / (max
+- min);
+    }
+   
+    private Color olderColor = Color.BLACK;
+   
+    public Color chooseColor(Color oldColor, int keyCode) {
+        /* option 1: INCREASING COLOUR
+        int r = oldColor.getRed();
+        int g = oldColor.getGreen();
+        int b = oldColor.getBlue();
+        if (r != 255)
+            r++;
+        if (g != 255)
+            g++;
+        if (b != 255)
+            b++;
+        */
+        /* option 2: Random colours
+        int r = ((keyCode % 3) / 2) * (keyCode % 255);
+        int g = (((keyCode - 1) % 3) / 2) * (keyCode % 255);
+        int b = (((keyCode - 2) % 3) / 2) * (keyCode % 255);
+        */
+        /* option 3: Old colours */
+        int r = olderColor.getGreen();
+        int g = oldColor.getBlue();
+        int b = keyCode;
+        olderColor = oldColor;
+        /**/
+        return new Color(r, g, b);       
+    }
+ 
+    public void keyReleased(KeyEvent ke) {
+        // ignore
+    }
+ 
+    public void keyTyped(KeyEvent ke) {
+        // ignore
+    }
+ 
+    public static void playNote(int note) {
+        Sequence sequence = null;
+        try {
+            sequence = new Sequence(Sequence.PPQ, 1);
+        } catch (InvalidMidiDataException e) {
+            e.printStackTrace();
+        }
+        Track track = sequence.createTrack();
+        ShortMessage message = new ShortMessage();
+        ShortMessage message2 = new ShortMessage();
+        try {
+            message.setMessage(ShortMessage.NOTE_ON, 0, note, 64);
+            message2.setMessage(ShortMessage.NOTE_OFF, 0, note, 64);
+        } catch (InvalidMidiDataException e) {
+            e.printStackTrace();
+        }
+        MidiEvent event = new MidiEvent(message, 0);
+        track.add(event);
+        event = new MidiEvent(message2, 1);
+        track.add(event);
+ 
+        // now play!
+        Sequencer sm_sequencer = null;
+        Synthesizer sm_synthesizer = null;
+       /*
+         * Now, we need a Sequencer to play the sequence. Here, we simply
+         * request the default sequencer.
+         */
+        try {
+            sm_sequencer = MidiSystem.getSequencer();
+        } catch (MidiUnavailableException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+ 
+        /*
+         * The Sequencer is still a dead object. We have to open() it to become
+         * live. This is necessary to allocate some ressources in the native
+         * part.
+         */
+        try {
+            sm_sequencer.open();
+        } catch (MidiUnavailableException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+ 
+        /*
+         * Next step is to tell the Sequencer which Sequence it has to play. In
+         * this case, we set it as the Sequence object created above.
+         */
+        try {
+            sm_sequencer.setSequence(sequence);
+        } catch (InvalidMidiDataException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+ 
+        /*
+         * Now, we set up the destinations the Sequence should be played on.
+         * Here, we try to use the default synthesizer. With some Java Sound
+         * implementations (Sun jdk1.3/1.4 and others derived from this
+         * codebase), the default sequencer and the default synthesizer are
+         * combined in one object. We test for this condition, and if it's true,
+         * nothing more has to be done. With other implementations (namely
+         * Tritonus), sequencers and synthesizers are always seperate objects.
+         * In this case, we have to set up a link between the two objects
+         * manually.
+         *
+         * By the way, you should never rely on sequencers being synthesizers,
+         * too; this is a highly non- portable programming style. You should be
+         * able to rely on the other case working. Alas, it is only partly true
+         * for the Sun jdk1.3/1.4.
+         */
+        if (!(sm_sequencer instanceof Synthesizer)) {
+            /*
+             * We try to get the default synthesizer, open() it and chain it to
+             * the sequencer with a Transmitter-Receiver pair.
+             */
+            try {
+                sm_synthesizer = MidiSystem.getSynthesizer();
+                sm_synthesizer.open();
+                Receiver synthReceiver = sm_synthesizer.getReceiver();
+                Transmitter seqTransmitter = sm_sequencer.getTransmitter();
+                seqTransmitter.setReceiver(synthReceiver);
+            } catch (MidiUnavailableException e) {
+                e.printStackTrace();
+            }
+        }
+       
+ 
+        /*
+         * There is a bug in the Sun jdk1.3/1.4. It prevents correct termination
+         * of the VM. So we have to exit ourselves. To accomplish this, we
+         * register a Listener to the Sequencer. It is called when there are
+         * "meta" events. Meta event 47 is end of track.
+         *
+         * Thanks to Espen Riskedal for finding this trick.
+         */
+        final Sequencer sm_sequencer2 = sm_sequencer;
+        final Synthesizer sm_synthesizer2 = sm_synthesizer;
+        sm_sequencer.addMetaEventListener(new MetaEventListener() {
+            public void meta(MetaMessage event) {
+                if (event.getType() == 47) {
+                    sm_sequencer2.close();
+                    if (sm_synthesizer2 != null) {
+                        sm_synthesizer2.close();
+                    }
+                }
+            }
+        });
+ 
+        /*
+         * Now, we can start over.
+         */
+        sm_sequencer.start();
+ 
+    }
+ 
 }
-
+ 
 class SimpleCanvas extends Canvas {
-
-    private Color color;
-
-    public SimpleCanvas(Color color) { 
-        this.color = color;
+ 
+    private Color color = Color.BLACK;
+ 
+    public Color getBackground() {
+        return this.color;
     }
-
+ 
     public void setBackground(Color color) {
         this.color = color;
         this.invalidate();
         this.repaint();
     }
-
-    public Color getBackground() {
-        return this.color;
-    }
-
 }
+
