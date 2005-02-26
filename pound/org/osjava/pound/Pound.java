628711fe2d0a74a2a5b243965ad593b84c8681b9
hen <hen@users.noreply.github.com>
2005-02-26 01:01:30 +0000

    changing background

--- pound/org/osjava/pound/Pound.java
+++ pound/org/osjava/pound/Pound.java
@@ -3,6 +3,9 @@ package org.osjava.pound;
 import java.awt.*;
 import java.awt.event.*;
 
+// plan: use javax.sound to play musical notes on key input
+//       have screen change based on keys hit; velocity of keys hit; show letter on screen
+//       show animal matching the particular letter and play animal noise
 public class Pound extends Frame implements KeyListener {
 
     public static void main(String[] args) {
@@ -10,31 +13,69 @@ public class Pound extends Frame implements KeyListener {
         p.show();
     }
 
+    private SimpleCanvas canvas;
+
     public Pound() {
         super("Pound");
         this.addKeyListener(this);
+        canvas = new SimpleCanvas(Color.BLACK);
+        this.add(canvas);
+        this.pack();
     }
 
 
     // KeyListener
-    public void keyPressed(KeyEvent ke) {
+    public void keyTyped(KeyEvent ke) {
         // ignore
+        ke.consume();
     }
 
     public void keyReleased(KeyEvent ke) {
         // ignore
+        ke.consume();
     }
 
-    public void keyTyped(KeyEvent ke) {
+    public void keyPressed(KeyEvent ke) {
         int keyCode = ke.getKeyCode();
 
-        if(keyCode == KeyEvent.VK_ESCAPE) {
+        if(keyCode == KeyEvent.VK_ESCAPE && ke.isShiftDown()) {
             System.err.println("Quitting. ");
+            ke.consume();
             this.dispose();
             System.exit(0);
         }
 
-        System.err.println("Pressed: "+KeyEvent.getKeyText(keyCode));
+        Color color = this.canvas.getBackground();
+        int r = color.getRed();
+        int g = color.getGreen();
+        int b = color.getBlue();
+        if(r != 255) r++;
+        if(g != 255) g++;
+        if(b != 255) b++;
+        this.canvas.setBackground(new Color(r,g,b));
+
+        System.err.println("Pressed: "+KeyEvent.getKeyText(keyCode)+" "+keyCode+" "+ke.getModifiers());
+        ke.consume();
+    }
+
+}
+
+class SimpleCanvas extends Canvas {
+
+    private Color color;
+
+    public SimpleCanvas(Color color) { 
+        this.color = color;
+    }
+
+    public void setBackground(Color color) {
+        this.color = color;
+        this.invalidate();
+        this.repaint();
+    }
+
+    public Color getBackground() {
+        return this.color;
     }
 
 }
