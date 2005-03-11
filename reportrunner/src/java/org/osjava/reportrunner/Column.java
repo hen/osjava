4ab8d741be7407a0387272cbbfa0d9d1c6f134bd
hen <hen@users.noreply.github.com>
2005-03-11 06:07:27 +0000

    added handy constructor, and helper factory method

--- reportrunner/src/java/org/osjava/reportrunner/Column.java
+++ reportrunner/src/java/org/osjava/reportrunner/Column.java
@@ -6,6 +6,18 @@ public class Column implements Nameable {
     private Formatter formatter;
     private String label;
 
+    /**
+     * Constructs a Column with a name and label of text. 
+     */
+    public Column(String text) {
+        this(text, text);
+    }
+
+    public Column(String name, String label) {
+        this.name = name;
+        this.label = label;
+    }
+
     public String getName() {
         return this.name;
     }
@@ -33,6 +45,16 @@ public class Column implements Nameable {
     // debug
     public String toString() { 
         return "Column { name="+name+", label="+label+" }";
+    }
+
+    // helper
+    public static Column[] createColumns(String[] names) {
+        Column[] columns = new Column[names.length];
+        int sz = columns.length;
+        for(int i=0; i < sz; i++) {
+            columns[i] = new Column(names[i]);
+        }
+        return columns;
     }
 
 
