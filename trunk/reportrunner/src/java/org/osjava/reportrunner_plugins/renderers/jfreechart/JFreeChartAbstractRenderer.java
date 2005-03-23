e5063239cd285e4baca0585761bfc39183e2507a
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2005-03-23 21:42:13 +0000

    slight code refactoring so I can nicely add a synchronized to deal with JFreeChart's well known synchronization problem

---
+++ trunk/reportrunner/src/java/org/osjava/reportrunner_plugins/renderers/jfreechart/JFreeChartAbstractRenderer.java
@@ -0,0 +1,39 @@
+package org.osjava.reportrunner_plugins.renderers.jfreechart;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.Writer;
+import java.io.*;
+
+import org.osjava.reportrunner.AbstractRenderer;
+import org.osjava.reportrunner.Report;
+import org.osjava.reportrunner.Result;
+
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+
+import org.apache.commons.lang.SerializationUtils;
+
+public class JFreeChartAbstractRenderer extends AbstractRenderer {
+
+    private JFreeChartCreator creator = null;
+
+    public void setCreator(String name) {
+        try {
+            Class c = Class.forName(name);
+            creator = (JFreeChartCreator) c.newInstance();
+        } catch(ClassNotFoundException cnfe) {
+        } catch(InstantiationException ie) {
+        } catch(IllegalAccessException iae) {
+        }
+    }
+
+	public void display(Result result, Report report, Writer out) throws IOException {
+		throw new RuntimeException("This should not be used with a Writer. ");
+	}
+
+	protected synchronized JFreeChart createChart(Result result, Report report) {
+        return this.creator.createChart(result, report);
+	}
+
+}
--- trunk/reportrunner/src/java/org/osjava/reportrunner_plugins/renderers/jfreechart/JFreeChartRenderer.java
+++ trunk/reportrunner/src/java/org/osjava/reportrunner_plugins/renderers/jfreechart/JFreeChartRenderer.java
@@ -14,26 +14,10 @@ import org.jfree.chart.JFreeChart;
 
 import org.apache.commons.lang.SerializationUtils;
 
-public class JFreeChartRenderer extends AbstractRenderer {
-
-    private JFreeChartCreator creator = null;
-
-    public void setCreator(String name) {
-        try {
-            Class c = Class.forName(name);
-            creator = (JFreeChartCreator) c.newInstance();
-        } catch(ClassNotFoundException cnfe) {
-        } catch(InstantiationException ie) {
-        } catch(IllegalAccessException iae) {
-        }
-    }
-
-	public void display(Result result, Report report, Writer out) throws IOException {
-		throw new RuntimeException("This should not be used with a Writer. ");
-	}
+public class JFreeChartRenderer extends JFreeChartAbstractRenderer {
 
 	public void display(Result result, Report report, OutputStream out)	throws IOException {
-		JFreeChart chart = this.creator.createChart(result, report);
+		JFreeChart chart = createChart(result, report);
         SerializationUtils.serialize(chart, out);
 	}
 
