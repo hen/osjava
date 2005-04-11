22f7c3e6bec03d2407404408475faec677785699
hen <hen@users.noreply.github.com>
2005-04-11 03:37:46 +0000

    Debugged, improved, still not working. Unable to find the vm files for some reason

--- reportrunner/src/java/org/osjava/webwizard/ControllerServlet.java
+++ reportrunner/src/java/org/osjava/webwizard/ControllerServlet.java
@@ -1,40 +1,49 @@
-packagr org.osjava.webwizard;
+package org.osjava.webwizard;
 
 import org.osjava.reportrunner.*;
+
+import org.apache.velocity.Template;
 import org.apache.velocity.servlet.VelocityServlet;
 import org.apache.velocity.app.FieldMethodizer;
-import org.apache.velocity.Context;
+import org.apache.velocity.context.Context;
+import org.apache.velocity.exception.ResourceNotFoundException;
+import org.apache.velocity.exception.ParseErrorException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class ControllerServlet extends VelocityServlet {
 
-    // expect to be a url of form:  http://foo.example.com/reports/velocity/list-groups.vm
-    public void handleRequest(HttpServletRequest request, HttpServletResponse response, Context ctxt) {
+    // expect to be a url of form:  http://foo.example.com/reports/controller/list-groups.vm
+    public Template handleRequest(HttpServletRequest request, HttpServletResponse response, Context ctxt) {
         // get path info such that the vm filename is known
-        String vmFilename = "";
+        String file = request.getPathInfo();
 
         ctxt.put("RRR", new FieldMethodizer( new Constants() ) );
 
-        // if list-groups
-        ReportGroup[] groups = ReportFactory.getReportGroups();
-        ctxt.put("groups", groups);
+        String groupName = request.getParameter(Constants.GROUP);
+        if(groupName == null || "".equals(groupName)) {
+            ReportGroup[] groups = ReportFactory.getReportGroups();
+            ctxt.put("groups", groups);
+        } else {
+            ReportGroup group = ReportFactory.getReportGroup(groupName);
+            ctxt.put("group", group);
+
+            String reportName = request.getParameter(Constants.REPORT);
+            if(reportName == null || "".equals(reportName)) {
+                Report[] reports = ReportFactory.getReports(groupName);
+                ctxt.put("reports", reports);
+            } else {
+                Report report = ReportFactory.getReport(groupName, reportName);
+                ctxt.put("report", report);
+            }
+        }
+
+        // if list-groups 
 
         // if list-reports
-        String groupName = request.getParameter(Constants.GROUP);
-        ReportGroup group = ReportFactory.getReportGroup(groupName);
-        ctxt.put("group", group);
-        Report[] reports = ReportFactory.getReports(groupName);
-        ctxt.put("reports", reports);
 
         // if enter-resource-params
-        String groupName = request.getParameter(Constants.GROUP);
-        ReportGroup group = ReportFactory.getReportGroup(groupName);
-        ctxt.put("group", group);
-        String reportName = request.getParameter(Constants.REPORT);
-        Report report = ReportFactory.getReport(groupName, reportName);
-        ctxt.put("report", report);
 
         // if enter-params
 
@@ -42,15 +51,21 @@ public class ControllerServlet extends VelocityServlet {
 
         // if list-renderers
 
-        String template = "org/osjava/webwizard/templates/"+vmFilename;
+        String template = "/org/osjava/webwizard/templates"+file+".vm";
         try {
             return getTemplate(template);
         } catch( ResourceNotFoundException rnfe ) {
             rnfe.printStackTrace();
             // couldn't find the template
+            throw new RuntimeException("Failed to find resource["+template+"]: " + rnfe);
         } catch( ParseErrorException pee ) {
             pee.printStackTrace();
             // syntax error : problem parsing the template
+            throw new RuntimeException("Failed to parse resource["+template+"]: " + pee);
+        } catch( Exception e ) {
+            e.printStackTrace();
+            // syntax error : general exception
+            throw new RuntimeException("FIX THIS: " + e);
         }
     }
 
