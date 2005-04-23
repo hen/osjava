585b415f300a19fc75cfb6b075c8986a45528986
rzigweid <rzigweid@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2005-04-23 12:45:27 +0000

    * Improved Javadocing * Made both methods properly handle (and throw) ClosedChannelException.  This   should resolve CT's nit about IOException.  It's not IllegalStateException,   it's actually looking like something better ;).  So I removed those comments   too. * Cleaned up various styling nits.  Yes I'm getting anal.

--- trunk/osjava-nio/src/java/org/osjava/nio/IOUtils.java
+++ trunk/osjava-nio/src/java/org/osjava/nio/IOUtils.java
@@ -1,95 +1,121 @@
+/*
+ * org.osjava.nio.IOUtils
+ *
+ * $Id$
+ * $URL$
+ * $Rev$
+ * $Date$
+ * $Author$
+ *
+ * Copyright (c) 2003-2005, Anthony Riley, Robert M. Zigweid
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * + Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * + Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * + Neither the name of the OSJava-NIO nor the names of its contributors may
+ *   be used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.osjava.nio;
 
-//import java.nio.channels.*;
 
 import java.io.IOException;
 
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 
-import java.nio.channels.SelectionKey;
+import java.nio.channels.ClosedChannelException;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 
 /**
- * Utility methods....
+ * Utility methods for the OSJava-NIO project.  This is the interface that
+ * is most useful for people to utilize.  They are intended to make the
+ * package easier to use without having to mess around with the inner workings
+ * of the package.
  *
- * These are the stuff most people will probably use, they're intended to
- * make using the package easier, without having to mess around with the
- * gubbings too much ;)
+ * @author Anthony Riley and Robert M. Zigweid
+ * @version $Rev$ $Date$
+
  */
 public class IOUtils {
 
     /**
-     * Listen on the specified port, create a new ServerSocketHandler wrapping
-     * the specified channel, register it with the specified IOThread
-     * and return it.
+     * Create a {@link ServerSocketChannelHandler} that is wrapped around a
+     * {@link ServerSocketChannel} listening to the address specified by
+     * <code>addr</code>.  The handler is registered with the specified
+     * {@link IOThread} before being returned.
+     *
+     * @param addr the address the ServerSocketChannel is bound to.
+     * @param thread the IOThread to register the SocketChannel with.
+     * @param acceptor The handler which is called when something connects
+     *        to the newly established ServerSocketChannel.
+     * @return a ServerSocketChannelHandler wrapped around the listening
+     *         listening ServerSocketChannel object.
+     * @throws ClosedChannelException if the underlying channel is closed.
+     * @throws IOException if an IO exception occurs.
      */
-    public static ServerSocketChannelHandler listen(
-            InetSocketAddress addr,
-            IOThread thread,
-            SocketChannelHandlerAcceptor acceptor
-            ) 
-        throws IOException
-        {
-            ServerSocketChannel chan = ServerSocketChannel.open();
-            ServerSocketChannelHandler handler = 
-                new ServerSocketChannelHandler(chan,thread);
-            int ops = chan.validOps();
-            SelectionKey key=null;
-
-            ServerSocket socket = chan.socket();
-            socket.bind(addr);
-
-            // Feckski Offski, I hate the way *everything* throws IOException
-            // It is *NOT* an IOException if you try and register a closed 
-            // channel, it is an IllegalStateException.
-            //
-            // Fuckwits.
-            try {
-                key = thread.register(handler, ops);
-            } catch (IOException ioe) {
-                throw new IllegalStateException("Underlying Channel is closed");
-            }
-
-            handler.setSocketChannelHandlerAcceptor(acceptor);
-            return handler;
+    public static ServerSocketChannelHandler listen(InetSocketAddress addr,
+                                                    IOThread thread,
+                                                    SocketChannelHandlerAcceptor acceptor)
+        throws ClosedChannelException, IOException {
+        ServerSocketChannel chan = ServerSocketChannel.open();
+        ServerSocketChannelHandler handler = new ServerSocketChannelHandler(chan,thread);
+        int ops = chan.validOps();
+        ServerSocket socket = chan.socket();
+        socket.bind(addr);
+        thread.register(handler, ops);
+        handler.setSocketChannelHandlerAcceptor(acceptor);
+        return handler;
         }
 
     /**
-     * Connect to the specified address, binding the stream to the created
-     * stream, (in a loop), and using the specified IOThread.
+     * Create a {@link SocketChannelHandler} that is wrapped around a
+     * {@link SocketChannel} listening to the address specified by
+     * <code>addr</code>.  The handler is registered with the specified
+     * {@link IOThread} before being returned.
      *
      * Note: There is no error handling here, if connectiong fails, I have
      * no clue what will happen, but this method will not throw an exception
      * as the underlying channel will be in a non-blocking mode, and hence
-     * .connect() will return immediately, and an error will be thrown
+     * <code>connect()</code> will return immediately, and an error will be thrown
      * when .finishConnect() is invoked by the SocketChannelHandler
+     *
+     * @param addr the address the SocketChannel is bound to.
+     * @param thread the IOThread to register the SocketChannel with.
+     * @return a SocketChannelHandler wrapped around the listening
+     *         listening ServerSocketChannel object.
+     * @throws ClosedChannelException if the underlying channel is closed.
+     * @throws IOException if an IO exception occurs.
      */
-    public static SocketChannelHandler connect(
-            InetSocketAddress addr,
-            IOThread thread) 
-        throws IOException 
-        {
-            SocketChannel chan = SocketChannel.open();
-            SocketChannelHandler handler = 
-                new SocketChannelHandler(chan,thread);
-            int ops = chan.validOps();
-            SelectionKey key=null;
-            
-            // Feckski Offski, I hate the way *everything* throws IOException
-            // It is *NOT* an IOException if you try and register a closed 
-            // channel, it is an IllegalStateException.
-            //
-            // Fuckwits.
-            try {
-                key = thread.register(handler, ops);
-            } catch (IOException ioe) {
-                throw new IllegalStateException("Underlying Channel is closed");
-            }
-
-            chan.connect(addr);
-
-            return handler;
-        }
+    public static SocketChannelHandler connect(InetSocketAddress addr,
+                                               IOThread thread)
+        throws ClosedChannelException, IOException {
+        SocketChannel chan = SocketChannel.open();
+        SocketChannelHandler handler = new SocketChannelHandler(chan,thread);
+        int ops = chan.validOps();
+        thread.register(handler, ops);
+        chan.connect(addr);
+        return handler;
+    }
 }
