79d511ff5f13bcfc9c549a81ad958f84c4d43264
rzigweid <rzigweid@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2005-04-23 12:16:06 +0000

    * Added javadocing for all methods and the class itself. * Changed register() to throw ClosedChannelException instead of IOException.   It's more specific this way. * Changed the exception thrown in register() from IllegalArgumentException to   IllegalStateException.  I think this is also more accurate.  Also added the   fact that ti's thrown to the method. * Removed all trailing white space. * Removed all debugging messages.  They aren't needed any longer (we hope).

--- osjava-nio/src/java/org/osjava/nio/IOThread.java
+++ osjava-nio/src/java/org/osjava/nio/IOThread.java
@@ -1,8 +1,48 @@
+/*
+ * org.osjava.nio.IOThread
+ *
+ * $Id$
+ * $URL$
+ * $Rev$
+ * $Date$
+ * $Author$
+ *
+ * Copyright (c) 2003-2005, Anthony Riley, Robert M. Zigweid
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * + Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * + Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * + Neither the name of the OSJava-NIO nor the names of its contributors may
+ *   be used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.osjava.nio;
 
 import java.io.IOException;
 import java.io.InterruptedIOException;
 
+import java.nio.channels.ClosedChannelException;
 import java.nio.channels.SelectableChannel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
@@ -12,25 +52,46 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
-import org.apache.log4j.Logger;
-
+/**
+ * A Thread facilitating the use of non-blocking
+ * {@link SelectableChannel SelectableChannels}.
+ * This Thread can be plugged in with the OSJava-Threads ThreadContext
+ * implementation.<p>
+ *
+ * For {@link Selector Selector's} that are registered with a Thread of
+ * this class, it is undefined if the {@link SelectionKey SelectionKeys}
+ * of the Selectors are modifified outside of the IOThread they are registered
+ * with.  This is an unfortunate side effect of synchronization issues.  While
+ * a Selector and a SelectionKey are both threadsafe, the key sets of a
+ * Selector are not.  Instead the {@link #addOps} and {@link #removeOps}
+ * methods are provided for this synchronization.
+ *
+ * @author Anthony Riley and Robert M. Zigweid
+ * @version $Rev$ $Date$
+ */
 public class IOThread extends Thread {
 
     private Selector mySelector;
 
-    /* Map for adding ops.  The key is the SelectionKey and the value 
-     * is the op to add.
+    /**
+     * Map for adding ops to keys.  The key is the SelectionKey and the
+     * value is the op to add.
      */
     private Map addOps = new HashMap();
-    
+
     /**
-     * Field containing the boolean value which indicates whether or not the 
-     * thread should abort.  This field exists for interoperability with 
-     * TigThreads ExtendedRunnable class.
+     * Map for removing ops from keys.  The key is the SelectionKey and
+     * the value is the op to remove.
+     */
+    private Map removeOps = new HashMap();
+
+    /**
+     * Field containing the boolean value which indicates whether or not the
+     * thread should abort.  This field exists for interoperability with
+     * OSJava-Threads ExtendedRunnable class.
      */
     private volatile boolean abort=false;
 
-    private Map removeOps = new HashMap();
 
     /**
      * Create a new IOThread.
@@ -66,120 +127,92 @@ public class IOThread extends Thread {
         super(group, name);
         mySelector = Selector.open();
     }
-    
+
+   /**
+    * Add an interestOp to the specified <code>key</code>.  This will
+    * queue the op for addition in the next cycle of the thread.
+    * Modification of keys outside of this mechanism is not recommended.
+    * Adding the op here, does not guarantee that the op will end up
+    * getting added to the key when the thread cycles.
+    *
+    * @param key the key to which the interest op will be added
+    * @param op the operation to add.
+    */
    public void addInterestOp(SelectionKey key, int op) {
-       int ops = key.readyOps();
-       Logger logger = Logger.getLogger(getClass());
-       logger.debug("Starting Ops for key '" + key + "' -- ");
-       if ((ops & SelectionKey.OP_ACCEPT) != 0) {
-           logger.debug("    ACCEPTING");
-       }
-       if ((ops & SelectionKey.OP_CONNECT) != 0) {
-           logger.debug("    CONNECTING");
-       }
-       if ((ops & SelectionKey.OP_READ) != 0) {
-           logger.debug("    READ");
-       }
-       if ((ops & SelectionKey.OP_WRITE) != 0) {
-           logger.debug("    WRITE");
-       }
-       logger.debug("Op being added -- ");
-       if ((op & SelectionKey.OP_ACCEPT) != 0) {
-           logger.debug("    ACCEPTING");
-       }
-       if ((op & SelectionKey.OP_CONNECT) != 0) {
-           logger.debug("    CONNECTING");
-       }
-       if ((op & SelectionKey.OP_READ) != 0) {
-           logger.debug("    READ");
-       }
-       if ((op & SelectionKey.OP_WRITE) != 0) {
-           logger.debug("    WRITE");
-       }
        if(!mySelector.keys().contains(key)) {
            /* TODO: Make this throw an exception */
-           logger.debug("Unknown key.  Aborting.");
            /* Wake up the selector anyway, just in case */
            mySelector.wakeup();
            return;
        }
        if(addOps.containsKey(key)) {
-           /* 
-            * If the key already exists in the addOps Map, bitwise OR the 
-            * value.  Unfortunately this has to be the Integer Object 
-            * rather than the primitive, so this looks worse than it 
+           /*
+            * If the key already exists in the addOps Map, bitwise OR the
+            * value.  Unfortunately this has to be the Integer Object
+            * rather than the primitive, so this looks worse than it
             * actually is.
             */
            addOps.put(key, new Integer(((Integer)(addOps.get(key))).intValue() | op));
        } else {
            addOps.put(key, new Integer(op));
-       } 
-       
+       }
+
        if(removeOps.containsKey(key)) {
            removeOps.put(key, new Integer(((Integer)(removeOps.get(key))).intValue() & ~op));
-       } 
-       logger.debug("Waking up selector");
-       mySelector.wakeup();       
+       }
+       mySelector.wakeup();
    }
 
+   /**
+    * Remove an interestOp from the specified <code>key</code>.  This will
+    * queue the op for removal in the next cycle of the thread.
+    * Modification of keys outside of this mechanism is not recommended.
+    * Removing the op here, does not guarantee that the op will end up
+    * getting added to the key when the thread cycles.
+    *
+    * @param key the key from which the interest op will be removed
+    * @param op the operation to remove.
+    */
    public void removeInterestOp(SelectionKey key, int op) {
        int ops = key.readyOps();
-       Logger logger = Logger.getLogger(getClass());
-       logger.debug("Removing Ops for key '" + key + "' -- ");
-       if ((ops & SelectionKey.OP_ACCEPT) != 0) {
-           logger.debug("    ACCEPTING");
-       }
-       if ((ops & SelectionKey.OP_CONNECT) != 0) {
-           logger.debug("    CONNECTING");
-       }
-       if ((ops & SelectionKey.OP_READ) != 0) {
-           logger.debug("    READ");
-       }
-       if ((ops & SelectionKey.OP_WRITE) != 0) {
-           logger.debug("    WRITE");
-       }
-       logger.debug("Op being removed -- ");
-       if ((op & SelectionKey.OP_ACCEPT) != 0) {
-           logger.debug("    ACCEPTING");
-       }
-       if ((op & SelectionKey.OP_CONNECT) != 0) {
-           logger.debug("    CONNECTING");
-       }
-       if ((op & SelectionKey.OP_READ) != 0) {
-           logger.debug("    READ");
-       }
-       if ((op & SelectionKey.OP_WRITE) != 0) {
-           logger.debug("    WRITE");
-       }
        if(!mySelector.keys().contains(key)) {
            /* TODO: Make this throw an exception */
-           logger.debug("Unknown key.  Aborting.");
            /* Wake up the selector anyway, just in case */
            mySelector.wakeup();
            return;
        }
        if(addOps.containsKey(key)) {
            addOps.put(key, new Integer(((Integer)(addOps.get(key))).intValue() & ~op));
-       } 
+       }
        if(removeOps.containsKey(key)) {
            removeOps.put(key, new Integer(((Integer)(removeOps.get(key))).intValue() | op));
        } else {
            removeOps.put(key, new Integer(ops));
        }
-       
-       logger.debug("Waking up selector");
-       mySelector.wakeup();       
+
+       mySelector.wakeup();
    }
 
    /**
-     * Register a ChannelHandler with this IOThread
+     * Register a {@link ChannelHandler} with the IOThread.  This serves to
+     * make the Thread's {@link Selector} aware of the {@link SelectionKey}
+     * the channel is associated with.  Handlers from blocking
+     * {@link SelectableChannel SelectableChannel's} cannot be registered.
+     *
+     * @param handler the ChannelHandler for the Selectable Channel
+     * @param ops the initial ops that the returned {@link SelectionKey} is
+     *        to be interested in.
+     * @return a SelectionKey to be associated with the <code>handler</code>
+     * @throws ClosedChannelException if the underlying channel is closed.
+     * @throws IllegalStateException if the underlying channel is a blocking
+     *         channel.
      */
-    public SelectionKey register(ChannelHandler handler, int ops) 
-        throws IOException {
+    public SelectionKey register(ChannelHandler handler, int ops)
+        throws ClosedChannelException, IllegalStateException {
         SelectableChannel chan = handler.getSelectableChannel();
 
         if (chan.isBlocking()) {
-            throw new IllegalArgumentException("SelectableChannel is blocking");
+            throw new IllegalStateException("SelectableChannel is blocking");
         }
 
         SelectionKey key = chan.register(mySelector, ops, handler);
@@ -191,21 +224,30 @@ public class IOThread extends Thread {
         }
         return key;
     }
-    
+
+    /**
+     * Deregisters a {@link ChannelHandler} <code>handler</code> from the thread.
+     * After deregistering, changes in the state of the thread's {@link
+     * SelectionKey} will be ignored.
+     *
+     * @param handler the ChannelHandler to deregister.
+     */
     public void deregister(ChannelHandler handler) {
         SelectionKey key=handler.getSelectableChannel().keyFor(mySelector);
-        
+
         if(key!=null) {
             key.cancel();
         }
     }
-    
+
+    /**
+     * The looping run method of the class.  All Threads have this.
+     */
     public void run() {
-        Logger logger = Logger.getLogger(getClass());
         while (!isAborting()) {
             try {
                 // XXX: Workaround, Selector.select() never returns if
-                // there is nothing to select on ! (even if you 
+                // there is nothing to select on ! (even if you
                 // call Selector.wakeup() )
                 synchronized (this) {
                     try {
@@ -213,32 +255,25 @@ public class IOThread extends Thread {
                             wait();
                         }
                     } catch (InterruptedException ie) {
-                        logger.debug("Breaking out of IOThread");
                         break;
                     }
                 }
-                
+
                 /* Look to apply ops to the keys first */
-                logger.debug("Setting interest ops");
                 Iterator it = addOps.keySet().iterator();
                 while(it.hasNext()) {
                     SelectionKey next = (SelectionKey)it.next();
-                    logger.debug("Setting an op");
                     int new_ops = ((Integer)addOps.get(next)).intValue();
                     next.interestOps(next.interestOps() | new_ops);
-                    logger.debug("Done setting an op");
                     /* Remove the key from the map. */
                     addOps.remove(next);
                 }
 
-                logger.debug("Removing interest ops");
                 it = removeOps.keySet().iterator();
                 while(it.hasNext()) {
                     SelectionKey next = (SelectionKey)it.next();
-                    logger.debug("Removing an op");
                     int new_ops = ((Integer)removeOps.get(next)).intValue();
                     next.interestOps(next.interestOps() & ~new_ops);
-                    logger.debug("Done removing an op");
                     /* Remove the key from the map. */
                     removeOps.remove(next);
                 }
@@ -246,14 +281,11 @@ public class IOThread extends Thread {
                 try {
                     boolean cont = true;
                     while(cont) {
-                        logger.debug("Preparing to select");
                         int selected;
                         selected = mySelector.select();
                         cont = (selected == 0);
-                        logger.debug("Done Selecting");
                     }
                 } catch (InterruptedIOException iie) {
-                    logger.debug("Breaking out of IOThread");
                     break;
                 }
                 Set keys = mySelector.selectedKeys();
@@ -264,22 +296,18 @@ public class IOThread extends Thread {
                     if (key.isValid()) {
                         int ops = key.readyOps();
                         if ((ops & SelectionKey.OP_ACCEPT) != 0) {
-                            logger.debug("ACCEPTING CONNECTION");
                             handler.accept();
                         }
                         if ((ops & SelectionKey.OP_CONNECT) != 0) {
-                            logger.debug("CONNECTING");
                             handler.connect();
                         }
                         if ((ops & SelectionKey.OP_READ) != 0) {
-                            logger.debug("READING FROM CONNECTION");
                             handler.readFromChannel();
-                            /* we always want to leave the reading as 
+                            /* we always want to leave the reading as
                              * an interested Op */
                             addInterestOp(key, SelectionKey.OP_READ);
                         }
                         if ((ops & SelectionKey.OP_WRITE) != 0) {
-                            logger.debug("WRITING TO CONNECTION");
                             handler.writeToChannel();
                         }
                     }
@@ -314,9 +342,10 @@ public class IOThread extends Thread {
     }
 
     /**
-     * Method to set the thread to cease execution gracefully.  This method 
-     * exists for interoperability with ExtendedRunnable from the TigThrads 
-     * package, but is in no way required.  
+     * Method to set the thread to cease execution gracefully.  This method
+     * exists for interoperability with ExtendedRunnable from the
+     * OSJava-Threads package, but is in no way required.
+     *
      * @param abort boolean value indicating whether or not the thread should
      *              cease execution.
      */
@@ -326,8 +355,9 @@ public class IOThread extends Thread {
 
     /**
      * Method returning whether or not the thread is in the process of ceasing
-     * to run.  This method exists for interoperability with ExtendedRunnable 
-     * from the TigThreads package, but is in no way required.
+     * to run.  This method exists for interoperability with ExtendedRunnable
+     * from the OSJava-Threads package, but is in no way required.
+     *
      * @return boolean value indicating whether or  not the thread is in the
      *         process of ceasing execution.
      */
