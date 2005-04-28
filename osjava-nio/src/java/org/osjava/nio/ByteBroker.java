7c9f34a12448e47ed6ecf9fd3b0e41f8a7ada753
cybertiger <cybertiger@users.noreply.github.com>
2005-04-28 10:24:00 +0000

    Additions and modified files to replace the writer interface.

---
+++ osjava-nio/src/java/org/osjava/nio/ByteBroker.java
@@ -0,0 +1,81 @@
+/*
+ * org.osjava.nio.ByteBroker
+ *
+ * $Id: ChannelHandler.java 1641 2005-04-23 20:23:14Z rzigweid $
+ * $URL: https://svn.osjava.org/svn/osjava/trunk/osjava-nio/src/java/org/osjava/nio/ChannelHandler.java $
+ * $Rev: 1641 $
+ * $Date: 2005-04-23 21:23:14 +0100 (Sat, 23 Apr 2005) $
+ * $Author: rzigweid $
+ *
+ * Copyright (c) 2003-2005, Anthony Riley, Robert M. Zigweid
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * + Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * + Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * + Neither the name of the OSJava-NIO nor the names of its contributors may
+ *   be used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.osjava.nio;
+
+import java.nio.ByteBuffer;
+
+/**
+ * @author cybertiger
+ *
+ * And interface to represent something which brokers bytes, either via
+ * the ByteBuffer class, or via arrays.
+ */
+public interface ByteBroker {
+
+    /**
+     * Do something with some bytes
+     *
+     * @param data Some bytes, this method is not guaranteed to use all the
+     * data, it is upto the caller to deal with any byte that are left
+     * in the ByteBuffer when this method returns.
+     * @param close Close socket on last write.
+     */
+    public void broker(ByteBuffer data, boolean close);
+
+    /**
+     * Do something with some bytes
+     *
+     * @param data Some bytes, this method is not guaranteed to use all the
+     * data, it is upto the caller to deal with any bytes that are not written
+     * @return the number of bytes brokered.
+     */
+    public int broker(byte[] data, boolean close);
+
+    /**
+     * Do something with some bytes
+     *
+     * @param data Some bytes, this method is not guaranteed to use all the
+     * data, it is upto the caller to deal with any bytes that are not written
+     * @param offset The offset into data where the bytes to broker are.
+     * @param len The number of bytes at the offset to broker.
+     * @return the number of bytes brokered.
+     */
+    public int broker(byte[] data, int offset, int len, boolean close);
+
+}
---
+++ osjava-nio/src/java/org/osjava/nio/CharToByteBroker.java
@@ -0,0 +1,121 @@
+/*
+ * org.osjava.nio.CharToByteBroker
+ *
+ * $Id: ChannelHandler.java 1641 2005-04-23 20:23:14Z rzigweid $
+ * $URL: https://svn.osjava.org/svn/osjava/trunk/osjava-nio/src/java/org/osjava/nio/ChannelHandler.java $
+ * $Rev: 1641 $
+ * $Date: 2005-04-23 21:23:14 +0100 (Sat, 23 Apr 2005) $
+ * $Author: rzigweid $
+ *
+ * Copyright (c) 2003-2005, Anthony Riley, Robert M. Zigweid
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * + Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * + Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * + Neither the name of the OSJava-NIO nor the names of its contributors may
+ *   be used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.osjava.nio;
+
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import java.nio.charset.CoderResult;
+
+/**
+ * @author cybertiger
+ *
+ */
+public class CharToByteBroker implements CharBroker {
+
+    private ByteBroker aBroker;
+
+    private CharsetEncoder encoder;
+
+    private ByteBuffer encodeBuffer;
+
+    public CharToByteBroker(ByteBroker aBroker) {
+        this(aBroker, Charset.forName("UTF-8").newEncoder());
+    }
+
+    public CharToByteBroker(ByteBroker aBroker, CharsetEncoder encoder) {
+        this(aBroker, encoder, 1024);
+    }
+
+    public CharToByteBroker(
+            ByteBroker aBroker, CharsetEncoder encoder, int bufSize
+            ) 
+    {
+        this.aBroker = aBroker;
+        this.encoder = encoder;
+        this.encodeBuffer = ByteBuffer.allocate(bufSize);
+    }
+
+    /* 
+     * This is where it actually happens, rest of the methods just call this
+     */
+    public void broker(CharBuffer data, boolean close) {
+        while(data.hasRemaining()) {
+            CoderResult result = encoder.encode(data, encodeBuffer, close);
+            /* TODO: need to handle encoder errors ! */
+            encodeBuffer.flip();
+            if(data.hasRemaining() && result.length() > 0) {
+                aBroker.broker(encodeBuffer, false);
+            } else {
+                aBroker.broker(encodeBuffer, close);
+            }
+            encodeBuffer.compact();
+            if(result.length() == 0) {
+                break; /* Can't encode anything else */
+            }
+        }
+    }
+
+    public int broker(char[] data, boolean close) {
+        CharBuffer buffer = CharBuffer.wrap(data);
+        broker(buffer,close);
+        return buffer.position();
+    }
+
+    public int broker(char[] data, int offset, int len, boolean close) {
+        CharBuffer buffer = CharBuffer.wrap(data);
+        broker(buffer,close);
+        return buffer.position() - offset;
+    }
+
+    public int broker(CharSequence str, boolean close) {
+        CharBuffer buffer = CharBuffer.wrap(str);
+        broker(buffer,close);
+        return buffer.position();
+    }
+
+    public int broker(CharSequence str, int offset, int len, boolean close) {
+        CharBuffer buffer = CharBuffer.wrap(str, offset, len);
+        broker(buffer,close);
+        return buffer.position() - offset;
+    }
+
+}
