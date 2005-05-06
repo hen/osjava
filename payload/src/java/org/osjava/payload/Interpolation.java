bbc2d83ba5744e329f0dc3077b509681b6961597
hen <hen@users.noreply.github.com>
2005-05-06 02:26:51 +0000

    package renaming

--- payload/src/java/org/osjava/payload/Interpolation.java
+++ payload/src/java/org/osjava/payload/Interpolation.java
@@ -29,7 +29,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
  * POSSIBILITY OF SUCH DAMAGE.
  */
-package com.generationjava.payload;
+package org.osjava.payload;
 
 import java.io.BufferedReader;
 import java.io.IOException;
