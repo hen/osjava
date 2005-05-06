39bf57da43c37617cc4f2dad178356d193443a97
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2005-05-06 02:16:39 +0000

    #PAY-15. Interface for Paylet plugin system

---
+++ trunk/payload/src/java/com/generationjava/payload/Paylet.java
@@ -0,0 +1,16 @@
+package com.generationjava.payload;
+
+import java.util.Properties;
+
+public interface Paylet {
+
+    /**
+     * Execute the paylet with the provided arguments. 
+     *
+     * @param args Arguments to the paylet, never null.
+     * @param payloadProperties The baked in payload.properties.
+     * @param properties The user supplied deployment properties. 
+     */
+    void execute(String[] args, Properties payloadProperties, Properties properties);
+
+}
