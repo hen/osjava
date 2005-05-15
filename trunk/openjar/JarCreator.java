79edf9381e8c336a01f4284ea58edec8034f3f3d
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2005-05-15 18:21:09 +0000

    bit of groundwork

--- trunk/openjar/JarCreator.java
+++ trunk/openjar/JarCreator.java
@@ -1,4 +1,31 @@
 import java.util.jar.*;
 
 public class JarCreator {
+
+    public static void main(String[] args) throws IOException {
+        String[] targets = new String[args.length - 1];
+        System.arraycopy(args, 1, targets, 0, targets.length);
+        new JarCreator(false).create(args[0], targets);
+    }
+
+    private boolean verbose;
+
+    public JarCreator() {
+    }
+
+    public JarCreator(boolean verbose) {
+        this.verbose = verbose;
+    }
+
+    public void create(String target, String[] filenames) throws IOException {
+        File[] files = new File[filenames.length];
+        for(int i=0; i<files.length; i++) {
+            files[i] = new File(filenames[i]);
+        }
+        create( new File(target), files );
+    }
+
+    public void create(File target, File[] files) throws IOException {
+    }
+
 }
