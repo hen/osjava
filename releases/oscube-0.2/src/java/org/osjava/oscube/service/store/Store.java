3f6413af18f0e44dafe795d8b13ec9fcaeb1d417
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2005-08-03 22:07:26 +0000

    Releasing 0.2 of OSCube

---
+++ releases/oscube-0.2/src/java/org/osjava/oscube/service/store/Store.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2003, Henri Yandell
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or 
+ * without modification, are permitted provided that the 
+ * following conditions are met:
+ * 
+ * + Redistributions of source code must retain the above copyright notice, 
+ *   this list of conditions and the following disclaimer.
+ * 
+ * + Redistributions in binary form must reproduce the above copyright notice, 
+ *   this list of conditions and the following disclaimer in the documentation 
+ *   and/or other materials provided with the distribution.
+ * 
+ * + Neither the name of OSJava nor the names of its contributors 
+ *   may be used to endorse or promote products derived from this software 
+ *   without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.osjava.oscube.service.store;
+
+import com.generationjava.config.Config;
+import org.osjava.oscube.container.Session;
+import org.osjava.oscube.container.Header;
+import org.osjava.oscube.container.Result;
+
+public interface Store {
+
+    /**
+     * Place the Result in a store of some kind. 
+     * This store does not have to be persistent, it could 
+     * be memory-based or the screen. 
+     */
+    public void store(Result result, Config cfg, Session session) throws StoringException;
+
+    /**
+     * Does this Header for a possible Result exist already?
+     * This is designed to try and stop parsing of stuff 
+     * already parsed, though it's not implemented yet. 
+     */
+    public boolean exists(Header header, Config cfg, Session session) throws StoringException;
+
+}
