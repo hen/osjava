8ea40ca925f9673432007530f87175395c7b45cc
hen <hen@users.noreply.github.com>
2005-08-16 04:10:58 +0000

    much prettying of the user-guide, nicely formatted now though a bit unsure about the general choice of CSS now I have to actually try and read this shit

--- scraping-engine/src/examples/package-example.sh
+++ scraping-engine/src/examples/package-example.sh
@@ -1,1 +1,3 @@
-tar -zcf scraping-examples-for-`grep current ../../project.xml  | head -1 | sed 's/<[^>]*>//g' | sed 's/ //g'`.tar.gz --exclude .svn Comics
+tar -zcf scraping-Comic-examples-for-`grep current ../../project.xml  | head -1 | sed 's/<[^>]*>//g' | sed 's/ //g'`.tar.gz --exclude .svn Comics
+tar -zcf scraping-Ftp-example-for-`grep current ../../project.xml  | head -1 | sed 's/<[^>]*>//g' | sed 's/ //g'`.tar.gz --exclude .svn Ftp
+tar -zcf scraping-template-for-`grep current ../../project.xml  | head -1 | sed 's/<[^>]*>//g' | sed 's/ //g'`.tar.gz --exclude .svn template
