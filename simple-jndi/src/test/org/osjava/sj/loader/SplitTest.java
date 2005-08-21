d485fe1bf8847e26ea2124143f1ffa3a7c0986ed
hen <hen@users.noreply.github.com>
2005-08-21 05:33:00 +0000

    testing the new custom split method

---
+++ simple-jndi/src/test/org/osjava/sj/loader/SplitTest.java
@@ -0,0 +1,33 @@
+package org.osjava.sj.loader;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+
+public class SplitTest extends TestCase {
+
+    public SplitTest(String name) {
+        super(name);
+    }
+
+    public void setUp() {
+    }
+
+    public void tearDown() {
+    }
+
+    public void testSplit() {
+        assertArrayEquals("", new String[] { "one", "two", "three"}, JndiLoader.split("one.two.three", ".") );
+        assertArrayEquals("", new String[] { "one"}, JndiLoader.split("one", ".") );
+        assertArrayEquals("", new String[] { ""}, JndiLoader.split("", ".") );
+    }
+
+    private void assertArrayEquals(String message, String[] array1, String[] array2) {
+        assertEquals(message+" - Length not equal ", array1.length, array2.length);
+        for(int i=0; i<array1.length; i++) {
+            assertEquals(message+" - array[" + i + "] element not equal ", array1[i], array2[i]);
+        }
+    }
+
+}
