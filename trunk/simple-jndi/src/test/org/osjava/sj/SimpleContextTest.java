18bd2cbe5d112cc2597e4ba6ebb7cb1810d3fe85
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2005-08-22 04:43:17 +0000

    removed the exception catching, let JUnit handle it for better stack traces. Xml and Ini both working now.

--- trunk/simple-jndi/src/test/org/osjava/sj/SimpleContextTest.java
+++ trunk/simple-jndi/src/test/org/osjava/sj/SimpleContextTest.java
@@ -29,56 +29,28 @@ public class SimpleContextTest extends TestCase {
         this.ctxt = null;
     }
 
-    public void testValueLookup() {
-        try {
-            assertEquals( "13", this.ctxt.lookup("test.value") );
-        } catch(NamingException ne) {
-            fail("NamingException: "+ne.getMessage());
-        }
+    public void testValueLookup() throws NamingException {
+        assertEquals( "13", this.ctxt.lookup("test.value") );
     }
 
-    /* Should return Context now I think?
-    public void testValuesLookup() {
-        try {
-            HashMap map = new HashMap();
-            map.put("five", "5");
-            map.put("one", "two");
-            map.put("three", "four");
-            assertEquals( map, this.ctxt.lookup("thing.type") );
-        } catch(NamingException ne) {
-            fail("NamingException: "+ne.getMessage());
-        }
-    }
-    */
-            
-    public void testListLookup() {
-        try {
-            ArrayList list = new ArrayList();
-            list.add( "24" );
-            list.add( "25" );
-            list.add( "99" );
-            assertEquals( list, this.ctxt.lookup("thing.type.bob.age") );
-        } catch(NamingException ne) {
-            fail("NamingException: "+ne.getMessage());
-        }
+    public void testListLookup() throws NamingException {
+        ArrayList list = new ArrayList();
+        list.add( "24" );
+        list.add( "25" );
+        list.add( "99" );
+        assertEquals( list, this.ctxt.lookup("thing.type.bob.age") );
     }
 
-    /*
-    public void testList2Lookup() {
-        try {
-            ArrayList list2 = new ArrayList();
-            list2.add( "Henri" );
-            list2.add( "Fred" );
-            assertEquals( list2, this.ctxt.lookup("name") );
-            assertEquals( "yandell.org", this.ctxt.lookup("url") );
-            assertEquals( "Foo", this.ctxt.lookup("com.genjava") );
-        } catch(NamingException ne) {
-            fail("NamingException: "+ne.getMessage());
-        }
+    public void testList2Lookup() throws NamingException {
+        ArrayList list2 = new ArrayList();
+        list2.add( "Henri" );
+        list2.add( "Fred" );
+        assertEquals( list2, this.ctxt.lookup("name") );
+        assertEquals( "yandell.org", this.ctxt.lookup("url") );
+        assertEquals( "Foo", this.ctxt.lookup("com.genjava") );
     }
 
-    public void testXmlLookup() {
-        try {
+    public void testXmlLookup() throws NamingException {
 //            System.err.println("XML: "+this.ctxt.lookup("xmltest") );
 // TODO: Should this return something? XML?
 //            System.err.println("XML: "+this.ctxt.lookup("xmltest.config") );
@@ -89,21 +61,12 @@ public class SimpleContextTest extends TestCase {
             list.add("one");
             list.add("two");
             assertEquals( list, this.ctxt.lookup("xmltest.config.multi.item") );
-        } catch(NamingException ne) {
-            fail("NamingException: "+ne.getMessage());
-        }
     }
 
-
-    public void testIniLookup() {
-        try {
-            assertEquals( "blockless", this.ctxt.lookup("testini/first") );
-            assertEquals( "13", this.ctxt.lookup("testini/block1/value") );
-            assertEquals( "pears", this.ctxt.lookup("testini/block2/apple") );
-            assertEquals( "stairs", this.ctxt.lookup("testini/block2/orange") );
-        } catch(NamingException ne) {
-            fail("NamingException: "+ne.getMessage());
-        }
+    public void testIniLookup() throws NamingException {
+        assertEquals( "blockless", this.ctxt.lookup("testini.first") );
+        assertEquals( "13", this.ctxt.lookup("testini.block1.value") );
+        assertEquals( "pears", this.ctxt.lookup("testini.block2.apple") );
+        assertEquals( "stairs", this.ctxt.lookup("testini.block2.orange") );
     }
-    */
 }
