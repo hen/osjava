25586c4905bd0677d4f1d66b15caa2a8e90f2e28
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2005-08-24 02:00:51 +0000

    works correctly from a system property setup instead of jndi.properties

---
+++ trunk/simple-jndi/src/test/org/osjava/sj/SystemPropertyTest.java
@@ -0,0 +1,37 @@
+package org.osjava.sj;
+
+import javax.naming.*;
+import java.util.*;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+
+public class SystemPropertyTest extends TestCase {
+
+    private InitialContext ctxt;
+
+    public SystemPropertyTest(String name) {
+        super(name);
+    }
+
+    public void setUp() {
+        System.setProperty("java.naming.factory.initial", "org.osjava.sj.SimpleContextFactory");
+        System.setProperty("org.osjava.sj.root", "file://src/test/config/system-test");
+        System.setProperty("org.osjava.sj.delimiter", "::::");
+        try {
+            ctxt = new InitialContext();
+        } catch(NamingException ne) {
+            ne.printStackTrace();
+        }
+    }
+
+    public void tearDown() {
+        this.ctxt = null;
+    }
+
+    public void testSystemPropertyContext() throws NamingException {
+        assertEquals( "1234", this.ctxt.lookup("one::::two::::three::::four") );
+    }
+}
