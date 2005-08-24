39576c1ce8bfd9a6dc33a1aaab3361fc65ea31dd
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2005-08-24 21:55:51 +0000

    only get new distributions

--- trunk/multidoc-jnr/getdoc.sh
+++ trunk/multidoc-jnr/getdoc.sh
@@ -8,11 +8,18 @@ do
   name=`echo $name | sed 's/\([^0-9]*\).*/\1/' | sed 's/-$//'`
   dest=$OUTPUT/$name/$version
 
-  mkdir -p $dest
-  unzip -q -d $dest $i *apidocs* 2>&1 | sed "s/caution: filename not matched:.*/No Javadoc in $name $version/"
-  mv $dest/*/docs/apidocs/* $dest 2>&1 | grep -v 'No such file or directory'
-  rmdir $dest/*/docs/apidocs 2>&1 | grep -v 'No such file or directory'
-  rmdir $dest/*/docs/ 2>&1 | grep -v 'No such file or directory'
-  rmdir $dest/*/ 2>&1 | grep -v 'Directory not empty' | grep -v 'No such file or directory'
-  rmdir $dest/ 2>&1 | grep -v 'Directory not empty' | grep -v 'No such file or directory'
+  if [ -d $dest ]
+  then
+    echo "Skipping $dest"
+  else
+    echo "Grabbing $dest"
+    mkdir -p $dest
+    unzip -q -d $dest $i *apidocs* 2>&1 | sed "s/caution: filename not matched:.*/No Javadoc in $name $version/"
+    mv $dest/*/docs/apidocs/* $dest 2>&1 | grep -v 'No such file or directory'
+    rmdir $dest/*/docs/apidocs 2>&1 | grep -v 'No such file or directory'
+    rmdir $dest/*/docs/ 2>&1 | grep -v 'No such file or directory'
+    rmdir $dest/*/ 2>&1 | grep -v 'Directory not empty' | grep -v 'No such file or directory'
+    rmdir $dest/ 2>&1 | grep -v 'Directory not empty' | grep -v 'No such file or directory'
+    rmdir $OUTPUT/$name | grep -v 'Directory not empty' | grep -v 'No such file or directory'
+  fi
 done
