e50c4331198efcedfbabe4032ee1e8287cfc4663
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2005-08-25 21:31:57 +0000

    improved Creator API so the renderer may be accessed

--- trunk/reportrunner/src/java/org/osjava/reportrunner_plugins/renderers/jfreechart/JFreeChartCreator.java
+++ trunk/reportrunner/src/java/org/osjava/reportrunner_plugins/renderers/jfreechart/JFreeChartCreator.java
@@ -2,11 +2,12 @@ package org.osjava.reportrunner_plugins.renderers.jfreechart;
 
 import org.osjava.reportrunner.Report;
 import org.osjava.reportrunner.Result;
+import org.osjava.reportrunner.Renderer;
 
 import org.jfree.chart.JFreeChart;
 
 public interface JFreeChartCreator {
 
-	JFreeChart createChart(Result result, Report report);
+	JFreeChart createChart(Result result, Report report, Renderer renderer);
 
 }
