b0550a875ef62105b703e1e254a38f953ef2bfbc
rzigweid <rzigweid@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2005-09-03 14:06:49 +0000

    Removed unused import.

--- trunk/simple-jndi/src/test/org/osjava/sj/loader/util/XmlPropertiesTest.java
+++ trunk/simple-jndi/src/test/org/osjava/sj/loader/util/XmlPropertiesTest.java
@@ -33,7 +33,6 @@
 package org.osjava.sj.loader.util;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.LinkedList;
