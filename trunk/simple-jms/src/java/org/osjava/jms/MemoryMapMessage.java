6ca9822bab683ebc904a9080864d964523ecb70e
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2005-09-06 18:23:00 +0000

    fleshing the message structure out

--- trunk/simple-jms/src/java/org/osjava/jms/MemoryMapMessage.java
+++ trunk/simple-jms/src/java/org/osjava/jms/MemoryMapMessage.java
@@ -1,54 +1,88 @@
 package org.osjava.jms;
-public class MemoryMapMessage implements MapMessage extends Message{
-    public boolean getBoolean(String);
-       throws JMSException
-    public byte getByte(String);
-       throws JMSException
-    public short getShort(String);
-       throws JMSException
-    public char getChar(String);
-       throws JMSException
-    public int getInt(String);
-       throws JMSException
-    public long getLong(String);
-       throws JMSException
-    public float getFloat(String);
-       throws JMSException
-    public double getDouble(String);
-       throws JMSException
-    public String getString(java.lang.String);
-       throws JMSException
-    public byte[] getBytes(String);
-       throws JMSException
-    public Object getObject(java.lang.String);
-       throws JMSException
-    public java.util.Enumeration getMapNames();
-       throws JMSException
-    public void setBoolean(String,boolean);
-       throws JMSException
-    public void setByte(String,byte);
-       throws JMSException
-    public void setShort(String,short);
-       throws JMSException
-    public void setChar(String,char);
-       throws JMSException
-    public void setInt(String,int);
-       throws JMSException
-    public void setLong(String,long);
-       throws JMSException
-    public void setFloat(String,float);
-       throws JMSException
-    public void setDouble(String,double);
-       throws JMSException
-    public void setString(String,java.lang.String);
-       throws JMSException
-    public void setBytes(String,byte[]);
-       throws JMSException
-    public void setBytes(String,byte[],int,int);
-       throws JMSException
-    public void setObject(String,java.lang.Object);
-       throws JMSException
-    public boolean itemExists(String);
-       throws JMSException
+
+import java.util.Enumeration;
+import java.util.Map;
+
+import java.jms.MapMessage;
+import java.jms.JMSException;
+
+public class MemoryMapMessage extends MemoryMessage implements MapMessage {
+
+    public boolean getBoolean(String key) throws JMSException {
+    }
+
+    public byte getByte(String key) throws JMSException {
+    }
+
+    public short getShort(String key) throws JMSException {
+    }
+
+    public char getChar(String key) throws JMSException {
+    }
+
+    public int getInt(String key) throws JMSException {
+    }
+
+    public long getLong(String key) throws JMSException {
+    }
+
+    public float getFloat(String key) throws JMSException {
+    }
+
+    public double getDouble(String key) throws JMSException {
+    }
+
+    public String getString(String key) throws JMSException {
+    }
+
+    public byte[] getBytes(String key) throws JMSException {
+    }
+
+    public Object getObject(String key) throws JMSException {
+    }
+
+    public Enumeration getMapNames() throws JMSException {
+    }
+
+    public void setBoolean(String key, boolean bool) throws JMSException {
+    }
+
+    public void setByte(String key, byte b) throws JMSException {
+    }
+
+    public void setShort(String key, short s) throws JMSException {
+    }
+
+    public void setChar(String key, char c) throws JMSException {
+    }
+
+    public void setInt(String key, int i) throws JMSException {
+    }
+
+    public void setLong(String key, long ln) throws JMSException {
+    }
+
+    public void setFloat(String key, float f) throws JMSException {
+    }
+
+    public void setDouble(String key, double d) throws JMSException {
+    }
+
+    public void setString(String key, String str) throws JMSException {
+    }
+
+    public void setBytes(String key, byte[] bytes) throws JMSException {
+    }
+
+    public void setBytes(String key, byte[] bytes, int offset, int length) throws JMSException {
+    }
+
+    public void setObject(String key, Object object) throws JMSException {
+    }
+
+    public boolean itemExists(String key) throws JMSException {
+    }
+
+
 }
 
--- trunk/simple-jms/src/java/org/osjava/jms/MemoryMessage.java
+++ trunk/simple-jms/src/java/org/osjava/jms/MemoryMessage.java
@@ -1,97 +1,145 @@
 package org.osjava.jms;
-interface Message{
-    public static final int DEFAULT_DELIVERY_MODE;
-    public static final int DEFAULT_PRIORITY;
-    public static final long DEFAULT_TIME_TO_LIVE;
-    public String getJMSMessageID();
-       throws JMSException
-    public void setJMSMessageID(String);
-       throws JMSException
-    public long getJMSTimestamp();
-       throws JMSException
-    public void setJMSTimestamp(long);
-       throws JMSException
-    public byte[] getJMSCorrelationIDAsBytes();
-       throws JMSException
-    public void setJMSCorrelationIDAsBytes(byte[]);
-       throws JMSException
-    public void setJMSCorrelationID(String);
-       throws JMSException
-    public String getJMSCorrelationID();
-       throws JMSException
-    public Destination getJMSReplyTo();
-       throws JMSException
-    public void setJMSReplyTo(Destination);
-       throws JMSException
-    public Destination getJMSDestination();
-       throws JMSException
-    public void setJMSDestination(Destination);
-       throws JMSException
-    public int getJMSDeliveryMode();
-       throws JMSException
-    public void setJMSDeliveryMode(int);
-       throws JMSException
-    public boolean getJMSRedelivered();
-       throws JMSException
-    public void setJMSRedelivered(boolean);
-       throws JMSException
-    public String getJMSType();
-       throws JMSException
-    public void setJMSType(String);
-       throws JMSException
-    public long getJMSExpiration();
-       throws JMSException
-    public void setJMSExpiration(long);
-       throws JMSException
-    public int getJMSPriority();
-       throws JMSException
-    public void setJMSPriority(int);
-       throws JMSException
-    public void clearProperties();
-       throws JMSException
-    public boolean propertyExists(String);
-       throws JMSException
-    public boolean getBooleanProperty(String);
-       throws JMSException
-    public byte getByteProperty(String);
-       throws JMSException
-    public short getShortProperty(String);
-       throws JMSException
-    public int getIntProperty(String);
-       throws JMSException
-    public long getLongProperty(String);
-       throws JMSException
-    public float getFloatProperty(String);
-       throws JMSException
-    public double getDoubleProperty(String);
-       throws JMSException
-    public String getStringProperty(java.lang.String);
-       throws JMSException
-    public Object getObjectProperty(java.lang.String);
-       throws JMSException
-    public java.util.Enumeration getPropertyNames();
-       throws JMSException
-    public void setBooleanProperty(String,boolean);
-       throws JMSException
-    public void setByteProperty(String,byte);
-       throws JMSException
-    public void setShortProperty(String,short);
-       throws JMSException
-    public void setIntProperty(String,int);
-       throws JMSException
-    public void setLongProperty(String,long);
-       throws JMSException
-    public void setFloatProperty(String,float);
-       throws JMSException
-    public void setDoubleProperty(String,double);
-       throws JMSException
-    public void setStringProperty(String,java.lang.String);
-       throws JMSException
-    public void setObjectProperty(String,java.lang.Object);
-       throws JMSException
-    public void acknowledge();
-       throws JMSException
-    public void clearBody();
-       throws JMSException
-}
 
+import javax.jms.Message;
+
+import java.util.Enumeration;
+
+public abstract class MemoryMessage implements Message {
+
+    public String getJMSMessageID() throws JMSException {
+    }
+
+    public void setJMSMessageID(String id) throws JMSException {
+    }
+
+    public long getJMSTimestamp() throws JMSException {
+    }
+
+    public void setJMSTimestamp(long timestamp) throws JMSException {
+    }
+
+    public byte[] getJMSCorrelationIDAsBytes() throws JMSException {
+    }
+
+    public void setJMSCorrelationIDAsBytes(byte[] bytes) throws JMSException {
+    }
+
+    public void setJMSCorrelationID(String id) throws JMSException {
+    }
+
+    public String getJMSCorrelationID() throws JMSException {
+    }
+
+    public Destination getJMSReplyTo() throws JMSException {
+    }
+
+    public void setJMSReplyTo(Destination replyTo) throws JMSException {
+    }
+
+    public Destination getJMSDestination() throws JMSException {
+    }
+
+    public void setJMSDestination(Destination destination) throws JMSException {
+    }
+
+    public int getJMSDeliveryMode() throws JMSException {
+    }
+
+    public void setJMSDeliveryMode(int mode) throws JMSException {
+    }
+
+    public boolean getJMSRedelivered() throws JMSException {
+    }
+
+    public void setJMSRedelivered(boolean isRedelivered) throws JMSException {
+    }
+
+    public String getJMSType() throws JMSException {
+    }
+
+    public void setJMSType(String type) throws JMSException {
+    }
+
+    public long getJMSExpiration() throws JMSException {
+    }
+
+    public void setJMSExpiration(long expiration) throws JMSException {
+    }
+
+    public int getJMSPriority() throws JMSException {
+    }
+
+    public void setJMSPriority(int priority) throws JMSException {
+    }
+
+    public void clearProperties() throws JMSException {
+    }
+
+    public boolean propertyExists(String key) throws JMSException {
+    }
+
+    public boolean getBooleanProperty(String key) throws JMSException {
+    }
+
+    public byte getByteProperty(String key) throws JMSException {
+    }
+
+    public short getShortProperty(String key) throws JMSException {
+    }
+
+    public int getIntProperty(String key) throws JMSException {
+    }
+
+    public long getLongProperty(String key) throws JMSException {
+    }
+
+    public float getFloatProperty(String key) throws JMSException {
+    }
+
+    public double getDoubleProperty(String key) throws JMSException {
+    }
+
+    public String getStringProperty(String key) throws JMSException {
+    }
+
+    public Object getObjectProperty(String key) throws JMSException {
+    }
+
+    public Enumeration getPropertyNames() throws JMSException {
+    }
+
+    public void setBooleanProperty(String key, boolean bool) throws JMSException {
+    }
+
+    public void setByteProperty(String key, byte b) throws JMSException {
+    }
+
+    public void setShortProperty(String key, short s) throws JMSException {
+    }
+
+    public void setIntProperty(String key, int i) throws JMSException {
+    }
+
+    public void setLongProperty(String key, long ln) throws JMSException {
+    }
+
+    public void setFloatProperty(String key, float f) throws JMSException {
+    }
+
+    public void setDoubleProperty(String key, double d) throws JMSException {
+    }
+
+    public void setStringProperty(String key, String str) throws JMSException {
+    }
+
+    public void setObjectProperty(String key, Object object) throws JMSException {
+    }
+
+    public void acknowledge() throws JMSException {
+    }
+
+    public void clearBody() throws JMSException {
+    }
+
+
+}
