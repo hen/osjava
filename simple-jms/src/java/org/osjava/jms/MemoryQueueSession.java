5551273e445dc82204eacfcfa37c74f4a27495c5
hen <hen@users.noreply.github.com>
2005-09-07 16:01:00 +0000

    not compilable yet, but closer

--- simple-jms/src/java/org/osjava/jms/MemoryQueueSession.java
+++ simple-jms/src/java/org/osjava/jms/MemoryQueueSession.java
@@ -1,18 +1,41 @@
 package org.osjava.jms;
-public class MemoryQueueSession implements QueueSession extends Session{
-    public Queue createQueue(String);
-       throws JMSException
-    public QueueReceiver createReceiver(javax.jms.Queue);
-       throws JMSException
-    public QueueReceiver createReceiver(javax.jms.Queue,String);
-       throws JMSException
-    public QueueSender createSender(javax.jms.Queue);
-       throws JMSException
-    public QueueBrowser createBrowser(javax.jms.Queue);
-       throws JMSException
-    public QueueBrowser createBrowser(javax.jms.Queue,String);
-       throws JMSException
-    public TemporaryQueue createTemporaryQueue();
-       throws JMSException
-}
 
+import javax.jms.JMSException;
+import javax.jms.QueueSession;
+import javax.jms.Queue;
+import javax.jms.TemporaryQueue;
+import javax.jms.QueueReceiver;
+import javax.jms.QueueBrowser;
+import javax.jms.QueueSender;
+
+public class MemoryQueueSession extends MemorySession implements QueueSession {
+
+    public Queue createQueue(String name) throws JMSException {
+        return new MemoryQueue(name);
+    }
+
+    public QueueReceiver createReceiver(Queue queue) throws JMSException {
+        return new MemoryQueueReceiver(queue);
+    }
+
+    public QueueReceiver createReceiver(Queue queue, String messageSelector) throws JMSException {
+        return new MemoryQueueReceiver(queue, messageSelector);
+    }
+
+    public QueueSender createSender(Queue queue) throws JMSException {
+        return new MemoryQueueSender(queue);
+    }
+
+    public QueueBrowser createBrowser(Queue queue) throws JMSException {
+        return new MemoryQueueBrowser(queue);
+    }
+
+    public QueueBrowser createBrowser(Queue queue, String messageSelector) throws JMSException {
+        return new MemoryQueueBrowser(queue, messageSelector);
+    }
+
+    public TemporaryQueue createTemporaryQueue() throws JMSException {
+        return new MemoryTemporaryQueue();
+    }
+
+}
