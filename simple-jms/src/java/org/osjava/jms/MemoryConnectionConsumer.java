8094ede6dd5e69ab202d16550bc0ff2b84233eeb
hen <hen@users.noreply.github.com>
2005-09-07 19:45:21 +0000

    compiles

--- simple-jms/src/java/org/osjava/jms/MemoryConnectionConsumer.java
+++ simple-jms/src/java/org/osjava/jms/MemoryConnectionConsumer.java
@@ -1,8 +1,20 @@
 package org.osjava.jms;
-interface ConnectionConsumer{
-    public ServerSessionPool getServerSessionPool();
-       throws JMSException
-    public void close();
-       throws JMSException
-}
 
+import javax.jms.ConnectionConsumer;
+import javax.jms.JMSException;
+import javax.jms.ServerSessionPool;
+
+// TODO: There are many optional parts of the API that 
+// do things with these, we're not implementing any yet
+public class MemoryConnectionConsumer implements ConnectionConsumer {
+
+    public ServerSessionPool getServerSessionPool() throws JMSException {
+        // TODO: Implement this
+        return null;
+    }
+
+    public void close() throws JMSException {
+        // TODO: Implement this
+    }
+
+}
