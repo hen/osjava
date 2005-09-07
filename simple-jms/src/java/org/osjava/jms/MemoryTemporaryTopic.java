8235ff0cedc945a1a61bdef0919d7448ff0ceb5f
hen <hen@users.noreply.github.com>
2005-09-07 02:16:45 +0000

    more implemented classes

--- simple-jms/src/java/org/osjava/jms/MemoryTemporaryTopic.java
+++ simple-jms/src/java/org/osjava/jms/MemoryTemporaryTopic.java
@@ -1,6 +1,11 @@
 package org.osjava.jms;
-public class MemoryTemporaryTopic implements TemporaryTopic extends Topic{
-    public void delete();
-       throws JMSException
+
+import javax.jms.TemporaryTopic;
+import javax.jms.JMSException;
+
+public class MemoryTemporaryTopic extends MemoryTopic implements TemporaryTopic {
+    public void delete() throws JMSException {
+        // TODO: Tell someone I'm dead
+    }
 }
 
