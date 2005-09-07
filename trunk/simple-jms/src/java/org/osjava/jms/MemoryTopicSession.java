4c4bf2ff2a35ef784f792fc18f58000b3b655182
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2005-09-07 16:01:00 +0000

    not compilable yet, but closer

--- trunk/simple-jms/src/java/org/osjava/jms/MemoryTopicSession.java
+++ trunk/simple-jms/src/java/org/osjava/jms/MemoryTopicSession.java
@@ -1,20 +1,45 @@
 package org.osjava.jms;
-public class MemoryTopicSession implements TopicSession extends Session{
-    public Topic createTopic(String);
-       throws JMSException
-    public TopicSubscriber createSubscriber(javax.jms.Topic);
-       throws JMSException
-    public TopicSubscriber createSubscriber(javax.jms.Topic,String,boolean);
-       throws JMSException
-    public TopicSubscriber createDurableSubscriber(javax.jms.Topic,String);
-       throws JMSException
-    public TopicSubscriber createDurableSubscriber(javax.jms.Topic,String,java.lang.String,boolean);
-       throws JMSException
-    public TopicPublisher createPublisher(javax.jms.Topic);
-       throws JMSException
-    public TemporaryTopic createTemporaryTopic();
-       throws JMSException
-    public void unsubscribe(String);
-       throws JMSException
-}
 
+import javax.jms.JMSException;
+import javax.jms.TemporaryTopic;
+import javax.jms.Topic;
+import javax.jms.TopicSubscriber;
+import javax.jms.TopicPublisher;
+import javax.jms.TopicSession;
+
+public class MemoryTopicSession implements TopicSession {
+
+    public Topic createTopic(String name) throws JMSException {
+        return new MemoryTopic(name);
+    }
+
+    public TopicSubscriber createSubscriber(Topic topic) throws JMSException {
+        return new MemoryTopicSubscriber(topic);
+    }
+
+    public TopicSubscriber createSubscriber(Topic topic, String messageSelector, boolean noLocals) throws JMSException {
+        return new MemoryTopicSubscriber(topic, messageSelector, noLocals);
+    }
+
+    public TopicSubscriber createDurableSubscriber(Topic topic, String name) throws JMSException {
+        return new MemoryTopicSubscriber(topic, name);
+    }
+
+    public TopicSubscriber createDurableSubscriber(Topic topic, String name, String messageSelector, boolean noLocals) throws JMSException {
+        return new MemoryTopicSubscriber(topic, name, messageSelector, noLocals);
+    }
+
+    public TopicPublisher createPublisher(Topic topic) throws JMSException {
+        return new MemoryTopicPublsher(topic);
+    }
+
+    public TemporaryTopic createTemporaryTopic() throws JMSException {
+        return new MemoryTemporaryTopic();
+    }
+
+    public void unsubscribe(String name) throws JMSException {
+        // TODO: Implement this
+    }
+
+
+}
