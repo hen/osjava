65c5fb650f6e0d4b2e9a6118f567c40f42d19168
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2005-09-07 14:40:33 +0000

    implemented to compile state

--- trunk/simple-jms/src/java/org/osjava/jms/MemoryServerSession.java
+++ trunk/simple-jms/src/java/org/osjava/jms/MemoryServerSession.java
@@ -1,8 +1,21 @@
 package org.osjava.jms;
-interface ServerSession{
-    public Session getSession();
-       throws JMSException
-    public void start();
-       throws JMSException
-}
 
+import javax.jms.JMSException;
+import javax.jms.ServerSession;
+import javax.jms.Session;
+
+public class MemoryServerSession implements ServerSession {
+
+    private Session session;
+
+    public Session getSession() throws JMSException {
+        // TODO: How does this get instantiated? Is it a MemorySession?
+        return this.session;
+    }
+
+    public void start() throws JMSException {
+        // TODO: Threading needed right?
+        this.session.run();
+    }
+
+}
