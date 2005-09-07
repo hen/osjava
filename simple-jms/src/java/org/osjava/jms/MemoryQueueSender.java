3fb306256a51b1e6ffd841a0964d6aa149660c18
hen <hen@users.noreply.github.com>
2005-09-07 19:53:27 +0000

    now compiling

--- simple-jms/src/java/org/osjava/jms/MemoryQueueSender.java
+++ simple-jms/src/java/org/osjava/jms/MemoryQueueSender.java
@@ -1,14 +1,38 @@
 package org.osjava.jms;
-public class MemoryQueueSender implements QueueSender extends MessageProducer{
-    public Queue getQueue();
-       throws JMSException
-    public void send(Message);
-       throws JMSException
-    public void send(Message,int,int,long);
-       throws JMSException
-    public void send(Queue,javax.jms.Message);
-       throws JMSException
-    public void send(Queue,javax.jms.Message,int,int,long);
-       throws JMSException
+
+import javax.jms.Message;
+import javax.jms.QueueSender;
+import javax.jms.Queue;
+import javax.jms.JMSException;
+
+public class MemoryQueueSender extends MemoryMessageProducer implements QueueSender {
+
+    private Queue queue;
+
+    public MemoryQueueSender(Queue queue) {
+        super(queue);
+        this.queue = queue;
+    }
+
+    public Queue getQueue() throws JMSException {
+        return this.queue;
+    }
+
+    public void send(Message msg) throws JMSException {
+        // TODO
+    }
+
+    public void send(Message message, int deliveryMode, int priority, long timeToLive) throws JMSException {
+        // TODO
+    }
+
+    public void send(Queue queue, Message msg) throws JMSException {
+        // TODO
+    }
+
+    public void send(Queue queue, Message message, int deliveryMode, int priority, long timeToLive) throws JMSException {
+        // TODO
+    }
+
 }
 
--- simple-jms/src/java/org/osjava/jms/MemoryTopicPublisher.java
+++ simple-jms/src/java/org/osjava/jms/MemoryTopicPublisher.java
@@ -1,14 +1,39 @@
 package org.osjava.jms;
-public class MemoryTopicPublisher implements TopicPublisher extends MessageProducer{
-    public Topic getTopic();
-       throws JMSException
-    public void publish(Message);
-       throws JMSException
-    public void publish(Message,int,int,long);
-       throws JMSException
-    public void publish(Topic,javax.jms.Message);
-       throws JMSException
-    public void publish(Topic,javax.jms.Message,int,int,long);
-       throws JMSException
+
+import javax.jms.TopicPublisher;
+import javax.jms.Topic;
+import javax.jms.JMSException;
+import javax.jms.Message;
+
+public class MemoryTopicPublisher extends MemoryMessageProducer implements TopicPublisher {
+
+    private Topic topic;
+
+    public MemoryTopicPublisher(Topic topic) {
+        super(topic);
+        this.topic = topic;
+    }
+
+    public Topic getTopic() throws JMSException {
+        return this.topic;
+    }
+
+    public void publish(Message msg) throws JMSException {
+        // TODO: Implement this
+    }
+
+    public void publish(Message message, int deliveryMode, int priority, long timeToLive) throws JMSException {
+        // TODO: Implement this
+    }
+
+    public void publish(Topic topic, Message msg) throws JMSException {
+        // TODO: Implement this
+    }
+
+    public void publish(Topic topic, Message message, int deliveryMode, int priority, long timeToLive) throws JMSException {
+        // TODO: Implement this
+    }
+
+
 }
 
