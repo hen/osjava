ce126c9a83b6afdab628df97a64be2ebb624f797
hen <hen@users.noreply.github.com>
2005-09-07 14:40:33 +0000

    implemented to compile state

--- simple-jms/src/java/org/osjava/jms/MemoryServerSessionPool.java
+++ simple-jms/src/java/org/osjava/jms/MemoryServerSessionPool.java
@@ -1,6 +1,13 @@
 package org.osjava.jms;
-interface ServerSessionPool{
-    public ServerSession getServerSession();
-       throws JMSException
-}
 
+import javax.jms.ServerSessionPool;
+import javax.jms.ServerSession;
+import javax.jms.JMSException;
+
+public class MemoryServerSessionPool implements ServerSessionPool {
+
+    public ServerSession getServerSession() throws JMSException {
+        throw new UnsupportedOperationException("Optional class not implemented. ");
+    }
+
+}
