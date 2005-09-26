bcafdb90df4afd55ad05ddd6ec0bd7f78c43248e
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2005-09-26 12:45:11 +0000

    Releasing 0.3

---
+++ releases/oscube-0.3/src/java/org/osjava/oscube/container/QuartzScheduler.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2003, Henri Yandell
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or 
+ * without modification, are permitted provided that the 
+ * following conditions are met:
+ * 
+ * + Redistributions of source code must retain the above copyright notice, 
+ *   this list of conditions and the following disclaimer.
+ * 
+ * + Redistributions in binary form must reproduce the above copyright notice, 
+ *   this list of conditions and the following disclaimer in the documentation 
+ *   and/or other materials provided with the distribution.
+ * 
+ * + Neither the name of OSJava nor the names of its contributors 
+ *   may be used to endorse or promote products derived from this software 
+ *   without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.osjava.oscube.container;
+
+import org.apache.commons.lang.StringUtils;
+
+import java.util.Date;
+
+import org.quartz.impl.StdSchedulerFactory;
+import org.quartz.JobDetail;
+import org.quartz.JobDataMap;
+import org.quartz.Job;
+//import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
+import org.quartz.CronTrigger;
+import org.quartz.SimpleTrigger;
+
+import java.text.ParseException;
+
+import org.apache.log4j.Logger;
+
+import com.generationjava.config.Config;
+
+/// BEWARE: Name clash. Scheduler and org.quartz.Scheduler
+public class QuartzScheduler implements Scheduler {
+
+    private static Logger logger = Logger.getLogger(QuartzScheduler.class);
+
+    private org.quartz.Scheduler quartz;
+
+    public QuartzScheduler() {
+        try {
+            quartz = new StdSchedulerFactory().getScheduler();
+            quartz.start();
+        } catch(SchedulerException se) {
+            throw new RuntimeException("Failed to schedule job. " + se);
+        }
+    }
+
+    public void schedule(Config cfg, Session session, Runner runner) {
+        // TODO: Should this clone, or the driver of this?
+        cfg = cfg.cloneConfig();
+        String schedule = cfg.getString("schedule");
+
+        // set default
+        if(StringUtils.isEmpty(schedule)) {
+            schedule = "startup";
+        } 
+
+        logger.debug("SCHEDULE: "+schedule);
+
+        // this should be one of the following:
+        // a cron-like line or a 'startup' option 
+        if("startup".equalsIgnoreCase(schedule)) { 
+            Scheduler sch = new SimpleScheduler();
+            sch.schedule(cfg, session, runner);
+        } else {
+            // assume it's a cron statement
+            try {
+                String ctxt = cfg.getContext();
+                String jobName = ctxt+"job";
+                // TODO: Make this 'Scraper' or some such. Maybe 
+                // a name that links to the current engine's instance
+                // Engine: +engine.toString()
+                String jobgroup = ctxt+"group"; 
+                JobDetail detail = new JobDetail(jobName, jobgroup, QuartzJob.class);
+                JobDataMap map = detail.getJobDataMap();
+                map.put("cfg", cfg);
+                map.put("session", session);
+                map.put("runner", runner);
+
+                Trigger trigger = null;
+                if("cron".equalsIgnoreCase(schedule)) {
+                    String cronTxt = cfg.getString("cron");
+                    logger.debug("Creating cron trigger: "+cronTxt+" for "+ctxt);
+                    CronTrigger cron = new CronTrigger(ctxt+"crontrigger", jobgroup, jobName, jobgroup, cronTxt);
+                    trigger = cron;
+                } else {
+                    // TODO: ie) "simple" in this case. need to make this explicit
+//                    Date start = cfg.getDate("simple.start");
+//                    Date end = cfg.getDate("simple.end");
+                    int repeat = cfg.getInt("simple.repeat");
+                    if(repeat == -1) {
+                        repeat = SimpleTrigger.REPEAT_INDEFINITELY;
+                    }
+                    int interval = cfg.getInt("simple.interval");
+                    logger.debug("Creating simple trigger: "+interval+":"+repeat+" for "+ctxt);
+                    SimpleTrigger simp = new SimpleTrigger(ctxt+"simpletrigger", jobgroup, repeat, interval);
+                    trigger = simp;
+                }
+
+                quartz.scheduleJob(detail, trigger);
+            } catch(ParseException pe) {
+                throw new RuntimeException("Failed to parse cron. " + pe);
+            } catch(SchedulerException se) {
+                throw new RuntimeException("Failed to schedule job. " + se);
+            }
+        }
+    }
+
+}
---
+++ releases/oscube-0.3/src/java/org/osjava/oscube/service/store/FileStore.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2003, Henri Yandell
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or 
+ * without modification, are permitted provided that the 
+ * following conditions are met:
+ * 
+ * + Redistributions of source code must retain the above copyright notice, 
+ *   this list of conditions and the following disclaimer.
+ * 
+ * + Redistributions in binary form must reproduce the above copyright notice, 
+ *   this list of conditions and the following disclaimer in the documentation 
+ *   and/or other materials provided with the distribution.
+ * 
+ * + Neither the name of OSJava nor the names of its contributors 
+ *   may be used to endorse or promote products derived from this software 
+ *   without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.osjava.oscube.service.store;
+
+import java.util.Iterator;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import java.net.URL;
+
+import com.generationjava.io.StreamW;
+
+import org.apache.log4j.Logger;
+
+import com.generationjava.config.Config;
+import org.osjava.oscube.container.Session;
+import org.osjava.oscube.container.Header;
+import org.osjava.oscube.container.Result;
+
+public class FileStore implements Store {
+
+    private static Logger logger = Logger.getLogger(FileStore.class);
+
+// FileStore assumes that the result contains a URL, InputStream, other?
+// Assumes only row(?)
+// TODO: If no saveAs, then it can handle multiple rows
+// TODO: Optional argument 'maintainExtension=true'. In which case the 
+//       saveAs does not need to mention the extension.
+    public void store(Result result, Config cfg, Session session) throws StoringException {
+        OutputStream out = null;
+        InputStream in = null;
+try {
+        String path = cfg.getString("file.path");
+        String saveAs = cfg.getString("file.saveAs");
+
+        File file = new File(path, saveAs);
+        FileOutputStream fos = new FileOutputStream(file);
+
+        Iterator iterator = result.iterateRows();
+        if(iterator.hasNext()) {
+            Object[] row = (Object[])iterator.next();
+            if(row.length == 0) {
+                logger.error("Empty row found. Skipping. ");
+                return;
+            }
+            in = open(row[0]);
+            if(in != null) {
+                StreamW.pushStream(in, fos);
+            }
+        }
+
+} catch(IOException ioe) {
+    throw new StoringException("File I/O Storing Error: "+ioe.getMessage(), ioe);
+} finally {
+        try { if(in != null) in.close(); } catch(IOException ioe) { } 
+        try { if(out != null) out.close(); } catch(IOException ioe) { } 
+}
+    }
+
+    public boolean exists(Header header, Config cfg, Session session) throws StoringException {
+        // need to write some kind of checking in here
+        return false;
+    }
+
+    private InputStream open(Object obj) throws IOException {
+        if(obj instanceof URL) {
+            URL url = (URL) obj;
+            return url.openStream();
+        } else 
+        if(obj instanceof String) {
+            return new java.io.ByteArrayInputStream( ( (String) obj ).getBytes() );
+        } else 
+        if(obj instanceof InputStream) {
+            return (InputStream) obj;
+        } else 
+        if(obj instanceof File) {
+            return new java.io.FileInputStream( (File) obj);
+        } else
+        if(obj instanceof byte[]) {
+            return new java.io.ByteArrayInputStream( (byte[]) obj);
+        } else {
+            return null;
+        }
+    }
+}
