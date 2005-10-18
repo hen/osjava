304cc17038890b68ae42143d86c46e2f0e1d7c8f
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2005-10-18 03:36:57 +0000

    Fixing minor warnings.

--- trunk/orcs/DataSourceWizard/src/java/org/osjava/dswiz/LoginPanel.java
+++ trunk/orcs/DataSourceWizard/src/java/org/osjava/dswiz/LoginPanel.java
@@ -9,7 +9,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
