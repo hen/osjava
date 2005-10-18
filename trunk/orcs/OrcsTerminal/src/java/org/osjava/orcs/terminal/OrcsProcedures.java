e74b8bc5bd1ed5aefee4cab0cb3a8fdbc9cec29b
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2005-10-18 03:35:23 +0000

    Removed unused imports.

--- trunk/orcs/OrcsTerminal/src/java/org/osjava/orcs/terminal/OrcsProcedures.java
+++ trunk/orcs/OrcsTerminal/src/java/org/osjava/orcs/terminal/OrcsProcedures.java
@@ -5,7 +5,6 @@ package org.osjava.orcs.terminal;
 
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
