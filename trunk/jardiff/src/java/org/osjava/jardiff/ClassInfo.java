230c04c203bdfde09ab50a61677702408150585a
cybertiger <cybertiger@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2005-11-06 17:10:25 +0000

    More javadocs (5/13 done, nearly halfway).

--- trunk/jardiff/src/java/org/osjava/jardiff/ClassInfo.java
+++ trunk/jardiff/src/java/org/osjava/jardiff/ClassInfo.java
@@ -39,15 +39,58 @@
 package org.osjava.jardiff;
 import java.util.Map;
 
+/**
+ * Information about a class file.
+ *
+ * @author Antony Riley <antony@cyberiantiger.org>
+ */
 public final class ClassInfo extends AbstractInfo
 {
+    /**
+     * The classfile version number.
+     */
     private int version;
+
+    /**
+     * The class signature.
+     */
     private String signature;
+
+    /**
+     * The internal classname of the superclass.
+     */
     private String supername;
+
+    /**
+     * An array of names of internal classnames of interfaces implmented 
+     * by the class.
+     */
     private String[] interfaces;
+
+    /**
+     * A map of method signature to MethodInfo, for the methods provided 
+     * by this class.
+     */
     private Map methodMap;
+
+    /**
+     * A map of field signature to FieldInfo, for the fields provided by 
+     * this class.
+     */
     private Map fieldMap;
     
+    /**
+     * Create a new classinfo.
+     *
+     * @param version the class file version number.
+     * @param access the access flags for the class.
+     * @param name the internal name of the class.
+     * @param signature the signature of the class.
+     * @param interfaces an array of internal names of interfaces implemented
+     *                   by the class.
+     * @param methodMap a map of methods provided by this class.
+     * @param fieldMap a map of fields provided by this class.
+     */
     public ClassInfo(int version, int access, String name, String signature,
                      String supername, String[] interfaces, Map methodMap,
                      Map fieldMap) {
@@ -60,26 +103,56 @@ public final class ClassInfo extends AbstractInfo
         this.fieldMap = fieldMap;
     }
     
+    /**
+     * Get the class file version.
+     *
+     * @return The class file version as specified in the java language spec.
+     */
     public final int getVersion() {
         return version;
     }
     
+    /**
+     * Get the class signature.
+     *
+     * @return the class signature
+     */
     public final String getSignature() {
         return signature;
     }
     
+    /**
+     * Get the internal name of the superclass.
+     *
+     * @return the internal name of the superclass
+     */
     public final String getSupername() {
         return supername;
     }
     
+    /**
+     * Get the internal names of the interfaces implemented by this class
+     *
+     * @return an array of internal names of classes implemented by the class.
+     */
     public final String[] getInterfaces() {
         return interfaces;
     }
     
+    /**
+     * Get the map of method signatures to methods.
+     *
+     * @return a map with method signatures as keys, and MethodInfos as values.
+     */
     public final Map getMethodMap() {
         return methodMap;
     }
     
+    /**
+     * Get the map of field signatures to fields.
+     *
+     * @return a map with field signatures as keys, and FieldInfos as values.
+     */
     public final Map getFieldMap() {
         return fieldMap;
     }
