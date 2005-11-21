8257977e52778d7390e64933e32d4dbdb049d24b
rzigweid <rzigweid@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2005-11-21 01:00:03 +0000

    ResultSetMetadata  * Stubbed getCatalogName to always return "" for now.  This will need to be    fixed at some later point when a strategy for how to approach it is    realized.  * Filled in some of the various column checks.  * Implemented getColumnType and getColumnTypeName

--- trunk/sqlite-jdbc/src/java/org/osjava/jdbc/sqlite/ResultSetMetaData.java
+++ trunk/sqlite-jdbc/src/java/org/osjava/jdbc/sqlite/ResultSetMetaData.java
@@ -41,6 +41,7 @@
 package org.osjava.jdbc.sqlite;
 
 import java.sql.SQLException;
+import java.sql.Types;
 
 /**
  * @author rzigweid
@@ -61,9 +62,9 @@ public class ResultSetMetaData implements java.sql.ResultSetMetaData {
     String[] columnNames = null;
     
     /**
-     * Array containing the declared types of columns for the ResultSet.
+     * Array containing the declared SQL types of columns for the ResultSet.
      */
-    String[] columnTypes = null;
+    String[] columnTypeNames = null;
 
     public ResultSetMetaData(ResultSet rs) {
         super();
@@ -81,6 +82,9 @@ public class ResultSetMetaData implements java.sql.ResultSetMetaData {
      * @see java.sql.ResultSetMetaData#isAutoIncrement(int)
      */
     public boolean isAutoIncrement(int column) throws SQLException {
+        if(column >= columnCount) {
+            throw new SQLException("Invalid column");
+        }
         // TODO Auto-generated method stub
         return false;
     }
@@ -89,6 +93,9 @@ public class ResultSetMetaData implements java.sql.ResultSetMetaData {
      * @see java.sql.ResultSetMetaData#isCaseSensitive(int)
      */
     public boolean isCaseSensitive(int column) throws SQLException {
+        if(column >= columnCount) {
+            throw new SQLException("Invalid column");
+        }
         // TODO Auto-generated method stub
         return false;
     }
@@ -97,6 +104,9 @@ public class ResultSetMetaData implements java.sql.ResultSetMetaData {
      * @see java.sql.ResultSetMetaData#isSearchable(int)
      */
     public boolean isSearchable(int column) throws SQLException {
+        if(column >= columnCount) {
+            throw new SQLException("Invalid column");
+        }
         // TODO Auto-generated method stub
         return false;
     }
@@ -105,6 +115,9 @@ public class ResultSetMetaData implements java.sql.ResultSetMetaData {
      * @see java.sql.ResultSetMetaData#isCurrency(int)
      */
     public boolean isCurrency(int column) throws SQLException {
+        if(column >= columnCount) {
+            throw new SQLException("Invalid column");
+        }
         // TODO Auto-generated method stub
         return false;
     }
@@ -113,6 +126,9 @@ public class ResultSetMetaData implements java.sql.ResultSetMetaData {
      * @see java.sql.ResultSetMetaData#isNullable(int)
      */
     public int isNullable(int column) throws SQLException {
+        if(column >= columnCount) {
+            throw new SQLException("Invalid column");
+        }
         // TODO Auto-generated method stub
         return 0;
     }
@@ -121,6 +137,9 @@ public class ResultSetMetaData implements java.sql.ResultSetMetaData {
      * @see java.sql.ResultSetMetaData#isSigned(int)
      */
     public boolean isSigned(int column) throws SQLException {
+        if(column >= columnCount) {
+            throw new SQLException("Invalid column");
+        }
         // TODO Auto-generated method stub
         return false;
     }
@@ -129,6 +148,9 @@ public class ResultSetMetaData implements java.sql.ResultSetMetaData {
      * @see java.sql.ResultSetMetaData#getColumnDisplaySize(int)
      */
     public int getColumnDisplaySize(int column) throws SQLException {
+        if(column >= columnCount) {
+            throw new SQLException("Invalid column");
+        }
         // TODO Auto-generated method stub
         return 0;
     }
@@ -137,6 +159,9 @@ public class ResultSetMetaData implements java.sql.ResultSetMetaData {
      * @see java.sql.ResultSetMetaData#getColumnLabel(int)
      */
     public String getColumnLabel(int column) throws SQLException {
+        if(column >= columnCount) {
+            throw new SQLException("Invalid column");
+        }
         // TODO Auto-generated method stub
         return null;
     }
@@ -145,6 +170,9 @@ public class ResultSetMetaData implements java.sql.ResultSetMetaData {
      * @see java.sql.ResultSetMetaData#getColumnName(int)
      */
     public String getColumnName(int column) throws SQLException {
+        if(column >= columnCount) {
+            throw new SQLException("Invalid column");
+        }
         /* column is 1 based, column names are 0 based. */
         return columnNames[column - 1];
     }
@@ -153,6 +181,9 @@ public class ResultSetMetaData implements java.sql.ResultSetMetaData {
      * @see java.sql.ResultSetMetaData#getSchemaName(int)
      */
     public String getSchemaName(int column) throws SQLException {
+        if(column >= columnCount) {
+            throw new SQLException("Invalid column");
+        }
         // TODO Auto-generated method stub
         return null;
     }
@@ -161,6 +192,9 @@ public class ResultSetMetaData implements java.sql.ResultSetMetaData {
      * @see java.sql.ResultSetMetaData#getPrecision(int)
      */
     public int getPrecision(int column) throws SQLException {
+        if(column >= columnCount) {
+            throw new SQLException("Invalid column");
+        }
         // TODO Auto-generated method stub
         return 0;
     }
@@ -169,6 +203,9 @@ public class ResultSetMetaData implements java.sql.ResultSetMetaData {
      * @see java.sql.ResultSetMetaData#getScale(int)
      */
     public int getScale(int column) throws SQLException {
+        if(column >= columnCount) {
+            throw new SQLException("Invalid column");
+        }
         // TODO Auto-generated method stub
         return 0;
     }
@@ -177,6 +214,9 @@ public class ResultSetMetaData implements java.sql.ResultSetMetaData {
      * @see java.sql.ResultSetMetaData#getTableName(int)
      */
     public String getTableName(int column) throws SQLException {
+        if(column >= columnCount) {
+            throw new SQLException("Invalid column");
+        }
         // TODO Auto-generated method stub
         return null;
     }
@@ -185,30 +225,104 @@ public class ResultSetMetaData implements java.sql.ResultSetMetaData {
      * @see java.sql.ResultSetMetaData#getCatalogName(int)
      */
     public String getCatalogName(int column) throws SQLException {
-        // TODO Auto-generated method stub
-        return null;
+        if(column >= columnCount) {
+            throw new SQLException("Invalid column");
+        }
+        /* TODO: This will need to be implemented at a later point, but the
+         *       utility of this method is not very high IMHO */
+        return "";
     }
 
     /* (non-Javadoc)
      * @see java.sql.ResultSetMetaData#getColumnType(int)
      */
     public int getColumnType(int column) throws SQLException {
-        // TODO Auto-generated method stub
-        return 0;
+        if(column >= columnCount) {
+            throw new SQLException("Invalid column");
+        }
+        /* This is based upon the type of the declaration type of the column.
+         * This approach was taken because of the way that SQLITE 3 deals with
+         * types.  Firstly, there are only 5 real types in the underlying 
+         * database, and even then they can be acquired as different types,
+         * usually strings.  This is going to produce a more accurae result.*/
+        /* Lowercase the declared type and use that as the key to everything.*/
+        String declType = columnTypeNames[column - 1].toLowerCase();
+        if(declType.equals("array")) {
+            return Types.ARRAY;
+        } else if(declType.equals("bigint")) {
+            return Types.BIGINT;
+        } else if(declType.equals("binary")) {
+            return Types.BINARY;
+        } else if(declType.equals("bit")) {
+            return Types.BIT;
+        } else if(declType.equals("blob")) {
+            return Types.BLOB;
+        } else if(declType.equals("boolean")) {
+            return Types.BOOLEAN;
+        } else if(declType.equals("char")) {
+            return Types.CHAR;
+        } else if(declType.equals("clob")) {
+            return Types.CLOB;
+        } else if(declType.equals("datalink")) {
+            return Types.DATALINK;
+       } else if(declType.equals("date")) {
+            return Types.DATE;
+        } else if(declType.equals("decimal")) {
+            return Types.DECIMAL;
+        } else if(declType.equals("double")) {
+            return Types.DOUBLE;
+        } else if(declType.equals("float")) {
+            return Types.FLOAT;
+        } else if(declType.equals("integer")) { 
+            return Types.INTEGER;
+        } else if(declType.equals("java_object")) {
+            return Types.JAVA_OBJECT;
+        } else if(declType.equals("longvarbinary")) {
+            return Types.LONGVARBINARY;
+        } else if(declType.equals("longvarchar")) {
+            return Types.LONGVARCHAR;
+        } else if(declType.equals("numeric")) {
+            return Types.NUMERIC;
+        } else if(declType.equals("real")) {
+            return Types.REAL;
+        } else if(declType.equals("ref")) {
+            return Types.REF;
+        } else if(declType.equals("smallint")) {
+            return Types.SMALLINT;
+        } else if(declType.equals("struct")) {
+            return Types.STRUCT;
+        } else if(declType.equals("time")) {
+            return Types.TIME;
+        } else if(declType.equals("timestamp")) {
+            return Types.TIMESTAMP;
+        } else if(declType.equals("tinyint")) {
+            return Types.TINYINT;
+        } else if(declType.equals("varbinary")) {
+            return Types.VARBINARY;
+        } else if(declType.startsWith("varchar")) {
+            return Types.VARCHAR;
+        } else {
+            return Types.OTHER;
+        }
     }
 
     /* (non-Javadoc)
      * @see java.sql.ResultSetMetaData#getColumnTypeName(int)
      */
     public String getColumnTypeName(int column) throws SQLException {
-        // TODO Auto-generated method stub
-        return null;
+        if(column >= columnCount) {
+            throw new SQLException("Invalid column");
+        }
+        return columnTypeNames[column - 1];
     }
 
     /* (non-Javadoc)
      * @see java.sql.ResultSetMetaData#isReadOnly(int)
      */
     public boolean isReadOnly(int column) throws SQLException {
+        if(column >= columnCount) {
+            throw new SQLException("Invalid column");
+        }
         // TODO Auto-generated method stub
         return false;
     }
@@ -217,6 +331,9 @@ public class ResultSetMetaData implements java.sql.ResultSetMetaData {
      * @see java.sql.ResultSetMetaData#isWritable(int)
      */
     public boolean isWritable(int column) throws SQLException {
+        if(column >= columnCount) {
+            throw new SQLException("Invalid column");
+        }
         // TODO Auto-generated method stub
         return false;
     }
@@ -225,6 +342,9 @@ public class ResultSetMetaData implements java.sql.ResultSetMetaData {
      * @see java.sql.ResultSetMetaData#isDefinitelyWritable(int)
      */
     public boolean isDefinitelyWritable(int column) throws SQLException {
+        if(column >= columnCount) {
+            throw new SQLException("Invalid column");
+        }
         // TODO Auto-generated method stub
         return false;
     }
@@ -233,6 +353,9 @@ public class ResultSetMetaData implements java.sql.ResultSetMetaData {
      * @see java.sql.ResultSetMetaData#getColumnClassName(int)
      */
     public String getColumnClassName(int column) throws SQLException {
+        if(column >= columnCount) {
+            throw new SQLException("Invalid column");
+        }
         // TODO Auto-generated method stub
         return null;
     }
@@ -249,7 +372,7 @@ public class ResultSetMetaData implements java.sql.ResultSetMetaData {
          */
         columnCount = count;
         columnNames = new String[columnCount];
-        columnTypes = new String[columnCount];
+        columnTypeNames = new String[columnCount];
     }
     
     /* Ugh, package private */
@@ -282,14 +405,14 @@ public class ResultSetMetaData implements java.sql.ResultSetMetaData {
     }
     
     /**
-     * Set the type of a column.  The value stored is a string directly from
-     * the declared type and will need to be parsed. 
+     * Set the SQL type of a column.  The value stored is a string directly
+     * from the declared type and will need to be parsed. 
      */
-    private void setColumnType(int col, String type) throws SQLException {
+    private void setColumnTypeName(int col, String type) throws SQLException {
         /* XXX: Note that column counts are 1 based in the api and 0 based here. */
         if(col >= columnCount) {
             throw new SQLException("Invalid column");
         }
-        columnTypes[col] = type;        
+        columnTypeNames[col] = type;
     }
 }
