d0da12fbc6aad2911b4d088ccc38b02966605ca7
hen <hen@users.noreply.github.com>
2006-01-03 19:13:19 +0000

    integrated jardiff

--- multidoc-jnr/genindex.sh
+++ multidoc-jnr/genindex.sh
@@ -1,18 +1,22 @@
-# DIR should be the location of the checked out svn.osjava.org/site/dist/ directory
-DIR=$1
-OUTPUT=$1/releases/multidoc-jnr
+# INPUT should be the location of the maven repository
+INPUT=$1
+# OUTPUT should be the location of the unzipped javadoc
+OUTPUT=$2
+
+mkdir -p $OUTPUT
 
 cp header-index-template.html $OUTPUT/index.html
 
 for i in `ls -1d $OUTPUT/*/ | sort -f`
 do
   i=`echo $i | sed 's/\/$//'`
+  echo -n "Starting $i"
   countI=`echo $i | wc -c`
   prettyI=`echo $i | sed 's/.*\///'`
 
   cp header-template.html $i/versions.html
 
-  for j in `ls -1d $i/*/ | versionsort`
+  for j in `ls -1d $i/*/ | ./versionsort`
   do
     j=`echo $j | sed 's/\/$//'`
     v=`echo $j | sed 's/.*\///'`
@@ -44,18 +48,22 @@ do
       then
         group='genjava'
       fi
-      new=$DIR/maven/$group/jars/$prettyI-$lastV.jar
-      old=$DIR/maven/$group/jars/$prettyI-$v.jar
+      new=$INPUT/$group/jars/$prettyI-$lastV.jar
+      old=$INPUT/$group/jars/$prettyI-$v.jar
       diff_file=$i/diff-report-${lastV}_${v}.html
       if [ -f $old -a -f $new ]
       then
-        echo "<TABLE BORDER=\"1\" WIDTH=\"100%\" CELLPADDING=\"3\" CELLSPACING=\"0\">" > $diff_file
-        echo "<TR BGCOLOR=\"#CCCCFF\" CLASS=\"TableHeadingColor\">" >> $diff_file
-        echo "<TD COLSPAN=3><FONT SIZE=\"+2\">" >> $diff_file
-        echo "<B>Changes Summary</B></FONT></TD></TR>" >> $diff_file
-#        java -jar clirr-core-0.5-uber.jar -o $old -n $new 2>&1 | sed 's/[^:]*:[^:]*: //' | sed 's/^/<tr class="TableRowColor"><td width="15%">/' | sed 's/:/<\/td><td>/' | sed 's/$/<\/td><\/tr>/' >> $diff_file
-        jardiff.pl $old $new 2>&1 | sed 's/[^:]*:[^:]*: //' >> $diff_file
-        echo "</table>" >> $diff_file
+#        echo "<TABLE BORDER=\"1\" WIDTH=\"100%\" CELLPADDING=\"3\" CELLSPACING=\"0\">" > $diff_file
+#        echo "<TR BGCOLOR=\"#CCCCFF\" CLASS=\"TableHeadingColor\">" >> $diff_file
+#        echo "<TD COLSPAN=3><FONT SIZE=\"+2\">" >> $diff_file
+#        echo "<B>Changes Summary</B></FONT></TD></TR>" >> $diff_file
+##        java -jar clirr-core-0.5-uber.jar -o $old -n $new 2>&1 | sed 's/[^:]*:[^:]*: //' | sed 's/^/<tr class="TableRowColor"><td width="15%">/' | sed 's/:/<\/td><td>/' | sed 's/$/<\/td><\/tr>/' >> $diff_file
+#        jardiff.pl $old $new 2>&1 | sed 's/[^:]*:[^:]*: //' >> $diff_file
+        java -jar jardiff-0.1-uber.jar $old $new > DIFF.xml
+        ant -q | grep -v 'BUILD SUCCESSFUL' | grep -v 'Total time:'
+        cat DIFF.html > $diff_file
+        rm DIFF.xml DIFF.html
+#        echo "</table>" >> $diff_file
         echo "<font class=\"FrameItemFont\"><a href=\"diff-report-${lastV}_${v}.html\" target="classFrame">(diff to ${v})</a></font>" >> $i/versions.html
       fi
       echo "<br/>" >> $i/versions.html
@@ -66,6 +74,7 @@ do
     echo "<nobr><a name=\"$v\"><font class=\"FrameItemFont\"><a href=\"javascript:load($u)\">$prettyI $v</a></font></nobr>" >> $i/versions.html
 
     lastV=$v
+    echo -n " $v "
   done
 
   unset lastV
@@ -76,6 +85,8 @@ do
   cp index-template.html $i/index.html
 
   echo "<li><a href=\"$prettyI/index.html\">$prettyI</a></li>" >> $OUTPUT/index.html
+
+  echo " ..... complete"
 
 done
 
