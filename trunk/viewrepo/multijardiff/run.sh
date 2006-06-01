87a426772601f5338477e83cc6bb7f62e22570b2
hen <hen@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2006-06-01 04:37:22 +0000

    You know...the created files aren't actually xml. Habit I guess. Switching to .dat

--- trunk/viewrepo/multijardiff/run.sh
+++ trunk/viewrepo/multijardiff/run.sh
@@ -10,9 +10,9 @@ do
         echo "Skipping ${name} - only one jar"
     else
         echo "Generating data for ${name}"
-        ./gen.rb $i/*.jar > multijardiff/${name}.xml
+        ./gen.rb $i/*.jar > multijardiff/${name}.dat
         echo "Rendering chart for ${name}"
-        java -classpath lib/jfreechart-1.0.1.jar:lib/jcommon-1.0.4.jar:. -Djava.awt.headless=true CreateChart multijardiff/${name}.xml "${name}" multijardiff/${name}.png
+        java -classpath lib/jfreechart-1.0.1.jar:lib/jcommon-1.0.4.jar:. -Djava.awt.headless=true CreateChart multijardiff/${name}.dat "${name}" multijardiff/${name}.png
     fi
 done
 
