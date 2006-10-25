cdb79946dd3c08d0ad8ada459429ffd66a5fcf7c
cybertiger <cybertiger@cf7a33d1-d123-0410-a11e-0b6f5a07fb8d>
2006-10-25 03:27:12 +0000

    Made the help->about menu item actually work.
    Added an about dialog, just a crude hack with a jpeg for now.

---
+++ trunk/mudclient/src/java/org/cyberiantiger/mudclient/ui/AboutDialog.java
@@ -0,0 +1,32 @@
+package org.cyberiantiger.mudclient.ui;
+
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+import java.awt.event.*;
+
+import org.cyberiantiger.mudclient.config.ClientConfiguration;
+
+public class AboutDialog extends JDialog {
+
+    private ImageIcon icon;
+    private JLabel logoLabel;
+
+    public AboutDialog(ControlWindow ctrl) {
+        super(ctrl, "About", false);
+        initComponents();
+    }
+
+    private void initComponents() {
+        icon = new ImageIcon(
+                AboutDialog.class.getClassLoader().getResource(
+                    "org/cyberiantiger/mudclient/about.jpg"
+                    ));
+        logoLabel = new JLabel(icon);
+
+        getContentPane().setLayout(new BorderLayout());
+        getContentPane().add(logoLabel, BorderLayout.CENTER);
+
+        pack();
+    }
+}
